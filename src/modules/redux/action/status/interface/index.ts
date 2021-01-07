f8c3ee8aa2587141dcd6e676e0e24f1157173004
ohjunsang <pandati@dsm.hs.kr>
2021-01-07 09:33:57 +0900

    [ADD]status action interface

---
+++ src/modules/redux/action/status/interface/index.ts
@@ -0,0 +1,4 @@
+export const STATUS = 'status/STATUS' as const;
+export const DESCRIPTION = 'status/DESCRIPTION' as const;
+export const IS_END = 'status/IS_END' as const;
+export const IS_START = 'status/IS_START' as const;
