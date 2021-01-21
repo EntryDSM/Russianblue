deb9fc138ce5731484a3ceacbfd34596669beb14
ohjunsang <pandati@dsm.hs.kr>
2021-01-21 20:21:55 +0900

    [ADD] signup

---
+++ src/containers/signup/index.ts
@@ -0,0 +1,2 @@
+import SignUpContainer from './signupContainer';
+export default SignUpContainer;
---
+++ src/containers/signup/signUpContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SignUp from '../../components/signup';
+
+const SignUpContainer = () => {
+  return <SignUp />;
+};
+
+export default SignUpContainer;
