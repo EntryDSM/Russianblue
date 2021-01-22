fbac9754c98ad3a8e408931b965088b75ca1b607
ohjunsang <pandati@dsm.hs.kr>
2021-01-22 11:38:33 +0900

    [ADD] checkboxhandler

--- src/components/signup/rule/SignUpRuleCheckbox.tsx
+++ src/components/signup/rule/SignUpRuleCheckbox.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { FC } from 'react';
 import * as S from '../style';
 
-const SignUpRuleCheckbox = () => {
+interface Props {
+  ruleCheck: boolean;
+  setRuleCheck: (payload: boolean) => void;
+}
+
+const SignUpRuleCheckbox: FC<Props> = ({ ruleCheck, setRuleCheck }) => {
+  const checkboxChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.checked;
+    setRuleCheck(value);
+  };
   return (
     <S.SignUpCheckboxWrapper>
       <S.SignUpCheckbox>
-        <input type='checkbox' />
+        <input type='checkbox' onChange={checkboxChangeHandler} checked={ruleCheck} />
         <div />
         <p>개인정보 이용약관를 확인했으며, 이에 동의합니다.</p>
       </S.SignUpCheckbox>
