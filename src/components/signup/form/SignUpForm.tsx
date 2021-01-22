a0715e8e329fa1e017611446ffb1c42f05108118
ohjunsang <pandati@dsm.hs.kr>
2021-01-22 11:38:02 +0900

    [ADD] handlers

--- src/components/signup/form/SignUpForm.tsx
+++ src/components/signup/form/SignUpForm.tsx
@@ -1,14 +1,45 @@
 import React, { FC } from 'react';
+import { isSignUpDataAble } from '../../../util/util';
 import * as S from '../style';
 import { NameColumn, PhoneNumberColumn, VertifyColumn, PasswordColumn } from './column';
 
-const SignUpForm: FC = () => {
+interface Props {
+  name: string;
+  phoneNumber: string;
+  phoneCode: string;
+  password: string;
+  ruleCheck: boolean;
+  setPassword: (payload: string) => void;
+  setPhoneCode: (payload: string) => void;
+  setName: (payload: string) => void;
+  setPhoneNumber: (payload: string) => void;
+}
+
+const SignUpForm: FC<Props> = ({
+  ruleCheck,
+  name,
+  phoneCode,
+  phoneNumber,
+  setPassword,
+  setPhoneCode,
+  setPhoneNumber,
+  setName,
+}) => {
   return (
     <S.SignUpContent>
-      <NameColumn disable={false} />
-      <PhoneNumberColumn disable={false} />
-      <VertifyColumn disable={true} />
-      <PasswordColumn disable={false} />
+      <NameColumn disable={!ruleCheck} setName={setName} />
+      <PhoneNumberColumn
+        disable={!ruleCheck || isSignUpDataAble(name)}
+        setPhoneNumber={setPhoneNumber}
+      />
+      <VertifyColumn
+        disable={!ruleCheck || isSignUpDataAble(name, phoneNumber)}
+        setPhoneCode={setPhoneCode}
+      />
+      <PasswordColumn
+        disable={!ruleCheck || isSignUpDataAble(name, phoneNumber, phoneCode)}
+        setPassword={setPassword}
+      />
     </S.SignUpContent>
   );
 };
