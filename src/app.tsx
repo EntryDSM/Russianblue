5c2db10c2b3027951dc6201c8bc45056c9c8ad2f
ohjunsang <pandati@dsm.hs.kr>
2021-01-22 11:43:27 +0900

    [ADD] wrap app component  with Provider for redux

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import GlobalStyle from './style/globalStyle';
 import RootRouter from './route';
+import { Provider } from 'react-redux';
+import store from './modules/redux/store';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <RootRouter />
-    </>
+    </Provider>
   );
 };
 
