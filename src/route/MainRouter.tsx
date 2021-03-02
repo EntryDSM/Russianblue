a3b9d315f8aefe3f170caa576293cf92a76b3da3
ohjunsang <pandati@dsm.hs.kr>
2021-03-02 11:09:09 +0900

    [ADD] modal container 를 router에 추가

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { MainContainer, SignUpContainer } from '../containers';
-import useHeader from '../util/hooks/useHeader';
-import useFooter from '../util/hooks/useFooter';
+import { useHeader, useFooter, useModal } from '../util/hooks/default';
 
 const MainRouter = () => {
   const Header = useHeader();
   const Footer = useFooter();
+  const Modal = useModal();
   return (
     <>
       {Header}
+      {Modal}
       <Switch>
         <Route path='/signup' component={SignUpContainer} />
         <Route exact path='/' component={MainContainer} />
