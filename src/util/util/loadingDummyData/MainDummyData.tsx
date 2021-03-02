dbb7d6f6e208b975ee78d41c504cbc5b3331fc86
ohjunsang <pandati@dsm.hs.kr>
2021-03-02 11:12:20 +0900

    [ADD] main에서 로딩중일때 보여질 데이터 추가

---
+++ src/util/util/loadingDummyData/MainDummyData.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { processType } from '../../../modules/redux/action/status/interface';
+
+const MainDummyData: processType = {
+  title: <p>로딩중...</p>,
+  description: <p>로딩중...</p>,
+  isHaveTerm: false,
+  isButtonAble: false,
+  buttonText: '로딩중...',
+};
+
+export default MainDummyData;
