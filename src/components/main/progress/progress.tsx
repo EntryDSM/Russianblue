560717f755ff6e06705b2407c8f66b8cab95aa1d
ohjunsang <pandati@dsm.hs.kr>
2021-03-02 10:55:35 +0900

    [FIX] process data 넣음

--- src/components/main/progress/progress.tsx
+++ src/components/main/progress/progress.tsx
@@ -1,17 +1,37 @@
 import React, { FC } from 'react';
 import * as S from '../style';
 import ProgressContent from './progressContent';
+import { mainProcessNumber } from '../../../modules/redux/reducer/status/mainConstance';
+interface Props {
+  status: string;
+}
 
-const Progress: FC = () => {
+const Progress: FC<Props> = ({ status }) => {
   return (
     <S.Progress>
-      <ProgressContent progressName='원서 접수' isNow={false} isPassed={true} />
+      <ProgressContent
+        progressName='원서 접수'
+        isNow={status === 'makeApplication'}
+        isPassed={mainProcessNumber[status] >= 2}
+      />
       <S.ProgressBlankBar />
-      <ProgressContent progressName='원서 접수' isNow={true} isPassed={false} />
+      <ProgressContent
+        progressName='1차 발표'
+        isNow={status === 'firstAnnouncement'}
+        isPassed={mainProcessNumber[status] >= 4}
+      />
       <S.ProgressBlankBar />
-      <ProgressContent progressName='원서 접수' isNow={false} isPassed={false} />
+      <ProgressContent
+        progressName='면접'
+        isNow={status === 'meeting'}
+        isPassed={mainProcessNumber[status] >= 5}
+      />
       <S.ProgressBlankBar />
-      <ProgressContent progressName='원서 접수' isNow={false} isPassed={false} />
+      <ProgressContent
+        progressName='발표 및 접수'
+        isNow={status === 'finalAnnouncement'}
+        isPassed={mainProcessNumber[status] >= 6}
+      />
       <div />
     </S.Progress>
   );
