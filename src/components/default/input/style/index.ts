e948c96f862e816428490eb49ede7b60adc01147
ohjunsang <pandati@dsm.hs.kr>
2021-03-02 10:22:06 +0900

    [ADD] password input style 추가

--- src/components/default/input/style/index.ts
+++ src/components/default/input/style/index.ts
@@ -1,5 +1,6 @@
 import { color, pxToRem } from '../../../../style';
 import styled, { css } from 'styled-components';
+import { eye, eye_active } from '../../../../assets/default';
 
 export const Input = styled.input<{
   width: number;
@@ -7,12 +8,56 @@ export const Input = styled.input<{
   disable: boolean;
   margin?: string;
 }>`
-  ${({ width, height, disable, margin }) => css`
+  outline: none;
+  box-sizing: border-box;
+  border: 1px solid ${color.fail};
+  padding: 0px 10px;
+  font-size: ${pxToRem(14)}rem;
+  ${({ width, height, margin }) => css`
     width: ${pxToRem(width)}rem;
     height: ${height ? pxToRem(height) : pxToRem(40)}rem;
-    outline: none;
+    margin: ${margin ? margin : ''};
+  `}
+`;
+
+export const PasswordInput = styled.div<{
+  width: number;
+  height?: number;
+  margin?: string;
+}>`
+  outline: none;
+  box-sizing: border-box;
+  ${({ width, height, margin }) => css`
+    width: ${pxToRem(width)}rem;
+    height: ${height ? pxToRem(height) : pxToRem(40)}rem;
     margin: ${margin ? margin : ''};
   `}
   border: 1px solid ${color.fail};
-  padding: 0px 10px;
+  padding: 0px 15px;
+  display: flex;
+  align-items: center;
+  > input {
+    width: ${({ width }) => pxToRem(width - 37)}rem;
+    height: 100%;
+    font-size: ${pxToRem(14)}rem;
+    border: none;
+    outline: none;
+    padding: 0px;
+  }
+  > label {
+    > input[type='checkbox'] {
+      display: none;
+    }
+    > input[type='checkbox']:checked ~ div {
+      background-image: url(${eye_active});
+      background-position: 0px 1px;
+    }
+    > div {
+      width: ${pxToRem(20)}rem;
+      height: ${pxToRem(16)}rem;
+      background-image: url(${eye});
+      background-size: contain;
+      background-repeat: no-repeat;
+    }
+  }
 `;
