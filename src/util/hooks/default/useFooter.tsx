56959425773ac156a4eb2c2458e5bf7765b825a1
ohjunsang <pandati@dsm.hs.kr>
2021-03-02 11:09:32 +0900

    [ADD] footer, header 사용하는 hooks 사용

--- src/util/hooks/default/useFooter.tsx
+++ src/util/hooks/default/useFooter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Footer } from '../../components/footer';
+import { Footer } from '../../../components/footer';
 
 const useFooter = () => {
   return <Footer />;
