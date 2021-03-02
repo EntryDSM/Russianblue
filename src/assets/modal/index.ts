2100ca2b17fc5027f56a2e8e7e63fb5e909e0f08
ohjunsang <pandati@dsm.hs.kr>
2021-03-02 10:15:41 +0900

    [ADD] assets

---
+++ src/assets/modal/index.ts
@@ -0,0 +1,6 @@
+export { default as move } from './move.svg';
+export { default as move_active } from './move_active.svg';
+export { default as password_reset_success } from './password_reset_success.svg';
+export { default as submit } from './submit.svg';
+export { default as file_error } from './file_error.svg';
+export { default as access_error } from './access_error.svg';
