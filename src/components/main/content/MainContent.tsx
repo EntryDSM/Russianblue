d0e0b18c5dfacd4440b3f14628367509c083c657
ohjunsang <pandati@dsm.hs.kr>
2021-03-05 11:05:41 +0900

    [FIX] 고정적인 text 였던 description에서 getDescription으로 date를 넣을 수 있게 수정

--- src/components/main/content/MainContent.tsx
+++ src/components/main/content/MainContent.tsx
@@ -3,21 +3,23 @@ import * as S from '../style';
 import MainButton from './MainButton';
 interface Props {
   title: string | React.ReactNode;
-  description: string | React.ReactNode;
+  getDescription: (date: string) => React.ReactNode;
   buttonText: string;
   isButtonAble: boolean;
   isHaveTerm: boolean;
   isLogin: boolean;
+  date: string;
   buttonClickHandler: () => void;
 }
 
 const MainContent: FC<Props> = ({
   title,
   buttonText,
-  description,
+  getDescription,
   isButtonAble,
   isHaveTerm,
   isLogin,
+  date,
   buttonClickHandler,
 }) => {
   return (
@@ -25,7 +27,7 @@ const MainContent: FC<Props> = ({
       <S.MainSubTitle>대덕소프트웨어마이스터고등학교</S.MainSubTitle>
       <S.MainTitle>2022년 신입생 모집</S.MainTitle>
       <S.MainDescription className='mainDescription'>{title}</S.MainDescription>
-      <S.MainDescription className='subDescription'>{description}</S.MainDescription>
+      <S.MainDescription className='subDescription'>{getDescription(date)}</S.MainDescription>
       {isLogin ? <MainButton>{buttonText}</MainButton> : <MainButton>로그인</MainButton>}
     </S.MainContentWrapper>
   );
