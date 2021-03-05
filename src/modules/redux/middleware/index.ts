99b43b9c7b98c13186995bc7733d468f0903084e
ohjunsang <pandati@dsm.hs.kr>
2021-03-05 11:13:32 +0900

    [ADD] saga 등록

--- src/modules/redux/middleware/index.ts
+++ src/modules/redux/middleware/index.ts
@@ -1,5 +1,7 @@
 import { all } from 'redux-saga/effects';
+import status from './status';
+import user from './user';
 
 export default function* rootSaga() {
-  yield all([]);
+  yield all([status, user]);
 }
