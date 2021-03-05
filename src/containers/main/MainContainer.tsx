1a884c06cbd381808e6a0f8c803e3f3c89bfe5b9
ohjunsang <pandati@dsm.hs.kr>
2021-03-05 11:11:48 +0900

    [ADD] date 추가

--- src/containers/main/MainContainer.tsx
+++ src/containers/main/MainContainer.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import MainDummyData from '../../util/util/loadingDummyData/MainDummyData';
 import Main from '../../components/main';
 import { useProcess } from '../../util/hooks/main';
 import { useAuth } from '../../util/hooks/auth';
+import { processTimeType } from 'src/modules/redux/reducer/status/interface';
 
 const MainContainer: FC = () => {
   const processState = useProcess();
@@ -11,9 +12,46 @@ const MainContainer: FC = () => {
     if (!processState.state.processes[status]) return MainDummyData;
     return processState.state.processes[status];
   };
+  useEffect(() => {
+    // server request로 대체 예정
+    processState.setState.setStatusSuccess([
+      {
+        year: '2022',
+        type: 'START_DATE',
+        date: '2022-10-31',
+      },
+      {
+        year: '2022',
+        type: 'END_DATE',
+        date: '2022-10-31',
+      },
+      {
+        year: '2022',
+        type: 'FIRST_ANNOUNCEMENT',
+        date: '2022-10-31',
+      },
+      {
+        year: '2022',
+        type: 'SECOND_ANNOUNCEMENT',
+        date: '2022-10-31',
+      },
+      {
+        year: '2022',
+        type: 'INTERVIEW',
+        date: '2022-10-31',
+      },
+    ]);
+  }, []);
+  const status = processState.state.status;
+  const date = processState.state.date;
+  const getNowProcessDate = (status): string => {
+    const result = date.filter(date => status === date.type)[0];
+    return result ? result.date : '';
+  };
   return (
     <Main
-      status={processState.state.status}
+      status={status}
+      date={getNowProcessDate(status)}
       process={getNowProcess(processState.state.status)}
       {...authState.state}
     />
