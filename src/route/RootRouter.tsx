94fb987dfa943353c60d9427a05522b2a63be94c
ohjunsang <pandati@dsm.hs.kr>
2021-03-15 14:41:24 +0900

    [ADD] add lazy loading

--- src/route/RootRouter.tsx
+++ src/route/RootRouter.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MainRouter from './MainRouter';
+
+const MainRouter = React.lazy(() => import('./MainRouter'));
 
 const RootRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={MainRouter} />
+        <Route
+          path='/'
+          render={() => (
+            <Suspense fallback={<div>Loading...</div>}>
+              <MainRouter />
+            </Suspense>
+          )}
+        />
       </Switch>
     </BrowserRouter>
   );
