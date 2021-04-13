a7caa40169442e09f0af4729e1dc74021cdcfebc
ohjunsang <pandati@dsm.hs.kr>
2021-04-13 21:53:32 +0900

    [FIX]

--- src/util/api/default/index.ts
+++ src/util/api/default/index.ts
@@ -1,21 +1,20 @@
 import axios from 'axios';
 
-const getRequest = () => {
+export const getRequest = () => {
   const request = axios.create({
     timeout: 10000,
     baseURL: process.env.BASE_URL,
   });
-  request.interceptors.response.use(
-    response => {
-      return response;
-    },
-    error => {
-      if (error.response.status === '401') {
-        // error handling code
-      }
-      return Promise.reject(error);
-    },
-  );
+  return request;
 };
 
-export default getRequest;
+export const getRequestWithAccessToken = (token: string) => {
+  const request = axios.create({
+    timeout: 10000,
+    baseURL: process.env.BASE_URL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return request;
+};
