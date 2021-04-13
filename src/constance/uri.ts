06bcffe2724033a265b8ea6c119ab32387a69c2f
ohjunsang <pandati@dsm.hs.kr>
2021-04-13 21:51:55 +0900

    [ADD] uri constance 추가

---
+++ src/constance/uri.ts
@@ -0,0 +1,11 @@
+enum uri {
+  signin = '/auth',
+  signup = '/user',
+  status = '/status',
+  sendVertify = '/phone/vertify',
+  sendPasswordVertify = '/password/phone/vertify',
+  resetPassword = '/password',
+  schedule = '/schedules',
+}
+
+export default uri;
