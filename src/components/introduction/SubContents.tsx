95d6319a02bcb43348acd7230c355f9247e19de1
hyejun <hyejun0608@naver.com>
2021-05-12 17:20:28 +0900

    [FIX] redux setState 추가

--- src/components/introduction/SubContents.tsx
+++ src/components/introduction/SubContents.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
+import useIntroduction from '../../util/hooks/Introduction';
 import * as S from './style';
+import { SELFINTRODUCTION, STUDYPLAN } from '../../constance/introduction';
 
 interface Props {
   subTitle: string;
@@ -7,14 +9,27 @@ interface Props {
 }
 
 const SubContent: FC<Props> = ({ subTitle, explain }) => {
+  const { state, setState } = useIntroduction();
+  const onIntroductionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setState.setIntroduction(e.target.value);
+  };
+  const onStudyPlanChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setState.setStudyPlan(e.target.value);
+  };
+
   return (
     <S.SubContent>
       <S.SubTitle>{subTitle}</S.SubTitle>
-      <S.Explain>
-        <span> </span>
-        {explain}
-      </S.Explain>
-      <S.TextBox />
+      <S.Explain>{explain}</S.Explain>
+      <S.TextBox>
+        {subTitle === SELFINTRODUCTION && <S.Textarea onChange={onIntroductionChange} />}
+        {subTitle === STUDYPLAN && <S.Textarea onChange={onStudyPlanChange} />}
+        <S.CountText>
+          <span>1600</span>
+          <span>/</span>
+          <span>0</span>
+        </S.CountText>
+      </S.TextBox>
     </S.SubContent>
   );
 };
