049b7775f9a027f2b9ba515def017ce3a3610da6
hyejun <hyejun0608@naver.com>
2021-05-12 22:29:44 +0900

    [FIX] 자기소개서, 학업계획서 작성시 글자 수 카운트

--- src/components/introduction/SubContents.tsx
+++ src/components/introduction/SubContents.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import useIntroduction from '../../util/hooks/Introduction';
 import * as S from './style';
 import { SELFINTRODUCTION, STUDYPLAN } from '../../constance/introduction';
@@ -10,11 +10,14 @@ interface Props {
 
 const SubContent: FC<Props> = ({ subTitle, explain }) => {
   const { state, setState } = useIntroduction();
+  const [countText, SetCountText] = useState({ introductionText: 0, studyPlanText: 0 });
   const onIntroductionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setState.setIntroduction(e.target.value);
+    SetCountText({ ...countText, introductionText: e.target.value.length });
   };
   const onStudyPlanChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setState.setStudyPlan(e.target.value);
+    SetCountText({ ...countText, studyPlanText: e.target.value.length });
   };
 
   return (
@@ -22,12 +25,15 @@ const SubContent: FC<Props> = ({ subTitle, explain }) => {
       <S.SubTitle>{subTitle}</S.SubTitle>
       <S.Explain>{explain}</S.Explain>
       <S.TextBox>
-        {subTitle === SELFINTRODUCTION && <S.Textarea onChange={onIntroductionChange} />}
-        {subTitle === STUDYPLAN && <S.Textarea onChange={onStudyPlanChange} />}
+        {subTitle === SELFINTRODUCTION && (
+          <S.Textarea onChange={onIntroductionChange} maxLength={1600} />
+        )}
+        {subTitle === STUDYPLAN && <S.Textarea onChange={onStudyPlanChange} maxLength={1600} />}
         <S.CountText>
           <span>1600</span>
           <span>/</span>
-          <span>0</span>
+          {subTitle === SELFINTRODUCTION && <span>{countText.introductionText}</span>}
+          {subTitle === STUDYPLAN && <span>{countText.studyPlanText}</span>}
         </S.CountText>
       </S.TextBox>
     </S.SubContent>
