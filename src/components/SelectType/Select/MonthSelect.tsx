08a5222693925ba21634f39c1b4843fc02e2efa6
hyejun <hyejun0608@naver.com>
2021-05-27 22:44:49 +0900

    [FIX] Select 클릭하면 선택 바가 나타남

--- src/components/SelectType/Select/MonthSelect.tsx
+++ src/components/SelectType/Select/MonthSelect.tsx
@@ -1,19 +1,44 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import * as S from '../style';
 import { dropdown, dropdown_active } from '../../../assets/selectType';
 
 interface Props {
-  active: number;
+  graduation_month: number;
+  setGraduationMonth: (payload: number) => void;
 }
 
-const MonthSelect: FC<Props> = ({ active }) => {
+const MonthSelect: FC<Props> = ({ graduation_month, setGraduationMonth }) => {
+  const [active, setActive] = useState(1);
+
+  const onSelectClick = () => {
+    if (active === 1) {
+      setActive(2);
+    } else {
+      setActive(1);
+    }
+  };
+
+  const onGraduationMonthClick = e => {
+    setGraduationMonth(e.target.innerText);
+  };
+
   return (
-    <S.Select width={105} disabled={active}>
+    <S.Select width={105} disabled={active} onClick={onSelectClick}>
       <S.SelectContent width={74}>
-        <p>12</p>
+        <p>{graduation_month}</p>
         {active === 1 && <img src={dropdown} />}
         {active === 2 && <img src={dropdown_active} />}
       </S.SelectContent>
+      {active === 2 && (
+        <S.SubSelect>
+          <S.GrayLine width={74} />
+          {Array(12)
+            .fill(1)
+            .map((e, i) => {
+              return <p onClick={onGraduationMonthClick}>{i + 1}</p>;
+            })}
+        </S.SubSelect>
+      )}
     </S.Select>
   );
 };
