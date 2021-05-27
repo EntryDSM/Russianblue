ff40e2a0cc8efac71adb119c9796cf3fb4576f2b
hyejun <hyejun0608@naver.com>
2021-05-27 22:48:04 +0900

    [FIX] select type action

---
+++ src/modules/redux/action/selectType/interface/index.ts
@@ -0,0 +1,9 @@
+export const TYPE = 'selectType/TYPE' as const;
+export const SOCIALTYPE = 'selectType/SOCIALTYPE' as const;
+export const AREA = 'selectType/AREA' as const;
+export const GRADUATION = 'selectType/GRADUATION' as const;
+export const GRADUATION_YEAR = 'selectType/GRADUATION_YEAR' as const;
+export const GRADUATION_MONTH = 'selectType/GRADUATION_MONTH' as const;
+export const REMARK = 'selectType/REMARK' as const;
+
+export const INPUT = 'selectType/INPUT' as const;
