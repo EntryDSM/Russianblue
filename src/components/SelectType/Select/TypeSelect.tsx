fc19c1e6d791b9ec8b8aea14645a4b9cddb8ed3b
hyejun <hyejun0608@naver.com>
2021-05-27 22:45:53 +0900

    [FIX] Select 클릭하면 선택 바가 나타남

--- src/components/SelectType/Select/TypeSelect.tsx
+++ src/components/SelectType/Select/TypeSelect.tsx
@@ -1,27 +1,44 @@
-import React, { FC } from 'react';
+import React, { Dispatch, FC, useState } from 'react';
 import * as S from '../style';
 import { dropdown, dropdown_active, dropdown_disabled } from '../../../assets/selectType';
 import { SOCIAL } from '../../../constance/SelectType';
 
 interface Props {
   disabled: number;
+  setDisabled: Dispatch<React.SetStateAction<number>>;
+  socialType: string;
+  setSocialType: (payload: string) => void;
 }
 
-const TypeSelect: FC<Props> = ({ disabled }) => {
+const TypeSelect: FC<Props> = ({ socialType, setSocialType, disabled, setDisabled }) => {
+  const onSelectClick = () => {
+    if (disabled === 1) {
+      setDisabled(2);
+    } else if (disabled === 2) {
+      setDisabled(1);
+    }
+  };
+
+  const onSocialTypeClick = e => {
+    setSocialType(e.target.innerText);
+  };
+
   return (
-    <S.Select width={167} disabled={disabled}>
+    <S.Select width={167} disabled={disabled} onClick={onSelectClick}>
       <S.SelectContent width={135}>
-        <p>사회통합전형</p>
+        <p>{socialType}</p>
         {disabled === 0 && <img src={dropdown_disabled} />}
         {disabled === 1 && <img src={dropdown} />}
         {disabled === 2 && <img src={dropdown_active} />}
       </S.SelectContent>
-      <S.SubSelect>
-        <S.GrayLine width={135} />
-        {SOCIAL.map(data => {
-          return <p>{data.content}</p>;
-        })}
-      </S.SubSelect>
+      {disabled === 2 && (
+        <S.SubSelect>
+          <S.GrayLine width={135} />
+          {SOCIAL.map(data => {
+            return <p onClick={onSocialTypeClick}>{data.content}</p>;
+          })}
+        </S.SubSelect>
+      )}
     </S.Select>
   );
 };
--- src/components/SelectType/Select/YearSelect.tsx
+++ src/components/SelectType/Select/YearSelect.tsx
@@ -1,19 +1,47 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import * as S from '../style';
 import { dropdown, dropdown_active } from '../../../assets/selectType';
 
 interface Props {
-  active: number;
+  disabled: number;
+  graduation_year: number;
+  setGraduationYear: (payload: number) => void;
 }
 
-const YearSelect: FC<Props> = ({ active }) => {
+const YearSelect: FC<Props> = ({ disabled, graduation_year, setGraduationYear }) => {
+  const [active, setActive] = useState(1);
+
+  const onSelectClick = () => {
+    if (disabled === 1) {
+      if (active === 1) {
+        setActive(2);
+      } else {
+        setActive(1);
+      }
+    }
+  };
+
+  const onGraduationYearClick = e => {
+    setGraduationYear(e.target.innerText);
+  };
+
   return (
-    <S.Select width={112} disabled={active}>
+    <S.Select width={112} disabled={disabled} onClick={onSelectClick}>
       <S.SelectContent width={80}>
-        <p>2022</p>
+        <p>{graduation_year}</p>
         {active === 1 && <img src={dropdown} />}
         {active === 2 && <img src={dropdown_active} />}
       </S.SelectContent>
+      {disabled === 1 && active === 2 && (
+        <S.SubSelect>
+          <S.GrayLine width={80} />
+          {Array(10)
+            .fill(1)
+            .map((e, i) => {
+              return <p onClick={onGraduationYearClick}>{i + 2016}</p>;
+            })}
+        </S.SubSelect>
+      )}
     </S.Select>
   );
 };
