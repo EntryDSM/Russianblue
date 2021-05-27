cd3794b0b9cd2e2061e335405fe6d9a768815344
hyejun <hyejun0608@naver.com>
2021-05-27 22:38:46 +0900

    [FIX] state, setState Props로 전달

--- src/components/SelectType/column/ChooseGraduationDate.tsx
+++ src/components/SelectType/column/ChooseGraduationDate.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import * as S from '../style';
 import { YearSelect, MonthSelect } from '../Select';
 import { GRADUATION_DATE_EXPLAIN } from '../../../constance/SelectType';
 
-const ChooseGraduationDate = () => {
+interface Props {
+  isToBe: boolean;
+  graduation_month: number;
+  graduation_year: number;
+  setGraduationYear: (payload: number) => void;
+  setGraduationMonth: (payload: number) => void;
+}
+
+const ChooseGraduationDate: FC<Props> = ({
+  isToBe,
+  graduation_month,
+  graduation_year,
+  setGraduationMonth,
+  setGraduationYear,
+}) => {
   return (
     <S.Line>
       <S.LineTitle>
         <span>*</span>졸업 연월
       </S.LineTitle>
-      <YearSelect active={1} />
+      <YearSelect
+        graduation_year={graduation_year}
+        setGraduationYear={setGraduationYear}
+        disabled={isToBe ? 3 : 1}
+      />
       <S.Date>년</S.Date>
-      <MonthSelect active={1} />
+      <MonthSelect graduation_month={graduation_month} setGraduationMonth={setGraduationMonth} />
       <S.Date>월</S.Date>
       <S.Explain>
         <span>*</span>
