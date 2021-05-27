9fa44b1ed7a86014b793999396fc166316258f9b
hyejun <hyejun0608@naver.com>
2021-05-27 22:46:27 +0900

    [FIX] redux state, setState props로 전달

--- src/components/SelectType/SelectType.tsx
+++ src/components/SelectType/SelectType.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { FC } from 'react';
 import * as S from './style';
 import { SCHOOL, SELECTTYPE } from '../../constance/SelectType';
 import Pagination from '../default/Pagination';
 import SelectLine from './SelectLine';
 
-const SelectType = () => {
+interface Props {
+  socialType: string;
+  area: string;
+  graduation: string;
+  graduation_month: number;
+  graduation_year: number;
+  remark: string;
+  setType: (payload: string) => void;
+  setSocialType: (payload: string) => void;
+  setArea: (payload: string) => void;
+  setGraduation: (payload: string) => void;
+  setGraduationYear: (payload: number) => void;
+  setGraduationMonth: (payload: number) => void;
+  setRemark: (payload: string) => void;
+}
+
+const SelectType: FC<Props> = ({
+  socialType,
+  area,
+  graduation,
+  graduation_month,
+  graduation_year,
+  remark,
+  setType,
+  setSocialType,
+  setArea,
+  setGraduation,
+  setGraduationMonth,
+  setGraduationYear,
+  setRemark,
+}) => {
   return (
     <S.SelectType>
       <div>
         <S.School>{SCHOOL}</S.School>
         <S.Title>{SELECTTYPE}</S.Title>
       </div>
-      <SelectLine />
+      <SelectLine
+        socialType={socialType}
+        area={area}
+        graduation={graduation}
+        graduation_month={graduation_month}
+        graduation_year={graduation_year}
+        remark={remark}
+        setType={setType}
+        setSocialType={setSocialType}
+        setArea={setArea}
+        setGraduation={setGraduation}
+        setGraduationMonth={setGraduationMonth}
+        setGraduationYear={setGraduationYear}
+        setRemark={setRemark}
+      />
       <Pagination nowPage={[true, false, false, false, false]} />
     </S.SelectType>
   );
