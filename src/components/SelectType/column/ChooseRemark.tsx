53b6c46702350b8fd539ee23528c23c72aaf4eb6
hyejun <hyejun0608@naver.com>
2021-05-27 22:39:59 +0900

    [FIX] 체크 버튼을 누를 시 색칠됨

--- src/components/SelectType/column/ChooseRemark.tsx
+++ src/components/SelectType/column/ChooseRemark.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import * as S from '../style';
 import { REMARKS, REMARK_EXPLAIN } from '../../../constance/SelectType';
 
-const ChooseRemark = () => {
+interface Props {
+  remark: string;
+  setRemark: (payload: string) => void;
+}
+
+const ChooseRemark: FC<Props> = () => {
+  const [isCheck, setIsCheck] = useState({ 1: false, 2: false });
+  const onCheckBtnClick = e => {
+    let dataId = Number(e.target.dataset.id);
+    switch (dataId) {
+      case 1:
+        setIsCheck({ 1: true, 2: false });
+        break;
+      case 2:
+        setIsCheck({ 1: false, 2: true });
+        break;
+    }
+  };
   return (
     <S.Line>
       <S.LineTitle>
-        <span></span>특기사항
+        <span />
+        특기사항
       </S.LineTitle>
       {REMARKS.map(data => {
         return (
           <S.SelectBox margin={65} key={data.id}>
-            <S.CheckCircle>
-              <S.CheckedCircle />
+            <S.CheckCircle onClick={onCheckBtnClick} data-id={data.id}>
+              {isCheck[data.id] && <S.CheckedCircle />}
             </S.CheckCircle>
             <p>{data.content}</p>
           </S.SelectBox>
