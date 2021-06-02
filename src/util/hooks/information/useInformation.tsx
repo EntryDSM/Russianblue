941f14f5d58bfcb71795b4a90d02590bf321bb25
hyejun <hyejun0608@naver.com>
2021-06-02 23:25:01 +0900

    [ADD] information custom hook

---
+++ src/util/hooks/information/useInformation.tsx
@@ -0,0 +1,34 @@
+import { useDispatch } from 'react-redux';
+import {
+  setInput,
+  setGender,
+  setBirthDate,
+  setBirthMonth,
+  setBirthYear,
+} from '../../../modules/redux/action/information';
+import { useSelectState } from '../default';
+
+const useInformation = () => {
+  const dispatch = useDispatch();
+  const state = useSelectState().information;
+  const setState = {
+    setInput: (payload: { name: string; value: string }) => {
+      dispatch(setInput(payload));
+    },
+    setGender: (payload: string) => {
+      dispatch(setGender(payload));
+    },
+    setBirthYear: (payload: number) => {
+      dispatch(setBirthYear(payload));
+    },
+    setBirthMonth: (payload: number) => {
+      dispatch(setBirthMonth(payload));
+    },
+    setBirthDate: (payload: number) => {
+      dispatch(setBirthDate(payload));
+    },
+  };
+  return { state, setState };
+};
+
+export default useInformation;
