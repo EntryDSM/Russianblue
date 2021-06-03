4977a6a9486a6f0039c9afc6265d380d09e8ce3e
hyejun <hyejun0608@naver.com>
2021-06-03 23:11:45 +0900

    [FIX] 이미지 업로드 시 미리보기 띄워주기

--- src/components/information/column/PictureBtn.tsx
+++ src/components/information/column/PictureBtn.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { FC, useMemo } from 'react';
 import * as S from '../style';
 import { IMG_EXPLAIN, IMG_FORMATS } from '../../../constance/information';
 import { imgIcon } from '../../../assets/Information';
 
-const PictureBtn = () => {
+interface Props {
+  imageUrl: string;
+  setImageUrl: (payload: string) => void;
+  setImageFile: (payload: File) => void;
+}
+
+const PictureBtn: FC<Props> = ({ setImageFile, setImageUrl, imageUrl }) => {
+  const fileChangeHandler= (e: React.ChangeEvent<HTMLInputElement>) => {
+    setImageFile(e.target.files[0]);
+    let filedes = URL.createObjectURL(e.target.files[0]);
+    setImageUrl(filedes);
+    console.log(e.target.files[0]);
+  }
+
+  const picture = useMemo(() => {
+    if(imageUrl) return <S.Picture src={imageUrl} />;
+    else return <>
+      <img src={imgIcon} />
+      <p>{IMG_EXPLAIN}</p>
+      <p>{IMG_FORMATS}</p>
+    </>;
+  }, [imageUrl]);
+
   return (
     <>
       <S.PictureBtn width={317} height={363} htmlFor='img'>
-        <img src={imgIcon} />
-        <p>{IMG_EXPLAIN}</p>
-        <p>{IMG_FORMATS}</p>
+        {picture}
       </S.PictureBtn>
       <input
         type='file'
         accept='.jpg, .jpge, .jpg2000, .png'
         id='img'
         style={{ display: 'none' }}
+        onChange={fileChangeHandler}
       />
     </>
   );
