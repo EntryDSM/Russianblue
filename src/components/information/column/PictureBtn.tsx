38ee98a003cda780d05e8fccc3a3091d0756a7e7
hyejun <hyejun0608@naver.com>
2021-06-04 16:37:43 +0900

    [FIX] width, height, padding 등 스타일 요서를 props로 전달

--- src/components/information/column/PictureBtn.tsx
+++ src/components/information/column/PictureBtn.tsx
@@ -4,31 +4,55 @@ import { IMG_EXPLAIN, IMG_FORMATS } from '../../../constance/information';
 import { imgIcon } from '../../../assets/Information';
 
 interface Props {
+  width: number;
+  height: number;
+  paddingTop: number;
+  paddingSide: number;
+  top: number;
   imageUrl: string;
   setImageUrl: (payload: string) => void;
   setImageFile: (payload: File) => void;
 }
 
-const PictureBtn: FC<Props> = ({ setImageFile, setImageUrl, imageUrl }) => {
-  const fileChangeHandler= (e: React.ChangeEvent<HTMLInputElement>) => {
+const PictureBtn: FC<Props> = ({
+  setImageFile,
+  setImageUrl,
+  imageUrl,
+  width,
+  height,
+  paddingTop,
+  paddingSide,
+  top,
+}) => {
+  const fileChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setImageFile(e.target.files[0]);
     let filedes = URL.createObjectURL(e.target.files[0]);
     setImageUrl(filedes);
     console.log(e.target.files[0]);
-  }
+  };
 
   const picture = useMemo(() => {
-    if(imageUrl) return <S.Picture src={imageUrl} />;
-    else return <>
-      <img src={imgIcon} />
-      <p>{IMG_EXPLAIN}</p>
-      <p>{IMG_FORMATS}</p>
-    </>;
+    if (imageUrl) return <S.Picture src={imageUrl} />;
+    else
+      return (
+        <>
+          <img src={imgIcon} />
+          <p>{IMG_EXPLAIN}</p>
+          <p>{IMG_FORMATS}</p>
+        </>
+      );
   }, [imageUrl]);
 
   return (
     <>
-      <S.PictureBtn width={317} height={363} htmlFor='img'>
+      <S.PictureBtn
+        width={width}
+        height={height}
+        paddingTop={paddingTop}
+        paddingSide={paddingSide}
+        top={top}
+        htmlFor='img'
+      >
         {picture}
       </S.PictureBtn>
       <input
