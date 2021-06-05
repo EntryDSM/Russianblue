4620201e894d32bee13f03bf07692a5b19f660fe
hyejun <hyejun0608@naver.com>
2021-06-05 23:50:34 +0900

    [FIX] 졸업자, 졸업예정자와 검정고시 보여주는 페이지 구분

--- src/components/information/Information.tsx
+++ src/components/information/Information.tsx
@@ -2,7 +2,9 @@ import React, { FC } from 'react';
 import * as S from './style';
 import Pagination from '../default/Pagination';
 import InformationForm from './InformationForm';
+import QualificationForm from './Qualification';
 import { SCHOOL, INFORMATIONTITLE } from '../../constance/information';
+import { useSelectType } from '../../util/hooks/selectType';
 
 interface Props {
   imageUrl: string;
@@ -31,25 +33,42 @@ const Information: FC<Props> = ({
   setImageFile,
   setImageUrl,
 }) => {
+  const { state } = useSelectType();
   return (
     <S.Information>
       <div>
         <S.School>{SCHOOL}</S.School>
         <S.Title>{INFORMATIONTITLE}</S.Title>
       </div>
-      <InformationForm
-        imageUrl={imageUrl}
-        birthYear={birthYear}
-        birthMonth={birthMonth}
-        birthDate={birthDate}
-        setInput={setInput}
-        setGender={setGender}
-        setBirthYear={setBirthYear}
-        setBirthMonth={setBirthMonth}
-        setBirthDate={setBirthDate}
-        setImageUrl={setImageUrl}
-        setImageFile={setImageFile}
-      />
+      {state.graduation === '검정고시' ? (
+        <QualificationForm
+          imageUrl={imageUrl}
+          birthYear={birthYear}
+          birthMonth={birthMonth}
+          birthDate={birthDate}
+          setInput={setInput}
+          setGender={setGender}
+          setBirthYear={setBirthYear}
+          setBirthMonth={setBirthMonth}
+          setBirthDate={setBirthDate}
+          setImageUrl={setImageUrl}
+          setImageFile={setImageFile}
+        />
+      ) : (
+        <InformationForm
+          imageUrl={imageUrl}
+          birthYear={birthYear}
+          birthMonth={birthMonth}
+          birthDate={birthDate}
+          setInput={setInput}
+          setGender={setGender}
+          setBirthYear={setBirthYear}
+          setBirthMonth={setBirthMonth}
+          setBirthDate={setBirthDate}
+          setImageUrl={setImageUrl}
+          setImageFile={setImageFile}
+        />
+      )}
       <Pagination nowPage={[false, true, false, false, false]} />
     </S.Information>
   );
