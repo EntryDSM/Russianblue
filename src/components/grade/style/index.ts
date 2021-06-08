796e136942cbeae00b960a983f6b4fd330ed16df
hyejun <hyejun0608@naver.com>
2021-06-08 17:15:06 +0900

    [FIX] 지원자 성적 입력 component style

--- src/components/grade/style/index.ts
+++ src/components/grade/style/index.ts
@@ -1,153 +1,199 @@
+import styled, { css } from 'styled-components';
 import { color } from '../../../style';
-import styled from 'styled-components';
 
 export const Grade = styled.div`
   width: 1220px;
-  min-height: 100vh;
-  margin: 0 auto;
   box-sizing: border-box;
+  margin: 0 auto;
+  padding-top: 125px;
   > div:first-child {
-    margin: 150px 0px 70px 0px;
+    margin-bottom: 70px;
   }
 `;
+
 export const School = styled.p`
-  width: 276px;
-  height: 34px;
   font-size: 20px;
   font-weight: 300;
 `;
+
 export const Title = styled.p`
-  width: 374px;
-  height: 46px;
   font-size: 35px;
   letter-spacing: 1.05px;
 `;
 
-// Volunteer
-export const Volunteer = styled.table`
-  width: 1220px;
-  border-top: 3px solid black;
-  border-bottom: 3px solid black;
-  > tr:first-child > td:nth-child(2) {
-    border-right: 1px solid #909090;
-  }
-  > tr:nth-child(2) > td:nth-child(2) {
-    border-right: 1px solid #909090;
-  }
-  > tr:nth-child(3) > td:first-child {
-    border-right: 1px solid #909090;
-  }
-`;
-export const ServingTime = styled.td`
-  width: 160px;
-  height: 70px;
+export const TableName = styled.p`
   font-size: 20px;
+  letter-spacing: -0.4px;
+  font-weight: 400;
+`;
+
+export const VolunteerTable = styled.div`
+  width: 1220px;
+  height: 210px;
+  border-top: 3px solid #000000;
+  border-bottom: 3px solid #000000;
+  display: flex;
+  margin-bottom: 77px;
+  > div:nth-child(2) > div {
+    display: flex;
+  }
+`;
+
+export const GradeTable = styled(VolunteerTable)`
+  height: 744px;
+  margin-bottom: 0px;
+  display: block;
+  > div:nth-child(2) > div {
+    display: flex;
+  }
+  > div:nth-child(2) > div:first-child {
+    display: block;
+  }
+`;
+
+export const TableTitle = styled.div<{
+  height: number;
+  padding: number;
+}>`
+  width: 160px;
   background-color: ${color.light};
-  padding-left: 43px;
+  font-size: 20px;
   box-sizing: border-box;
-`;
-export const Attendance = styled(ServingTime)`
-  height: 140px;
-  border-top: none;
-`;
-export const VolunteerTd = styled.td`
-  width: 533px;
-  border-bottom: 1px solid #909090;
-  font-size: 18px;
-  > div {
-    display: flex;
-    align-items: center;
-    margin-left: 32px;
-  }
-`;
-export const Input = styled.input`
-  width: 76px;
-  height: 36px;
-  border: 1px solid #606060;
-  font-size: 18px;
-  border-radius: 4px;
-  margin-right: 15px;
   text-align: center;
+  ${({ height, padding }) => css`
+    height: ${height}px;
+    padding: ${padding}px 0px;
+  `}
 `;
-export const DateInput = styled.div`
-  width: 424px;
+
+export const TableTd = styled.div<{
+  border?: boolean;
+}>`
+  width: 530px;
+  height: 70px;
   display: flex;
-  align-items: center;
   justify-content: space-between;
-  > div {
-    display: flex;
-    align-items: center;
-  }
-`;
-export const EnterGradeTitle = styled.div`
-  width: 100%;
-  display: flex;
   align-items: center;
-  justify-content: space-between;
-  font-size: 16px;
-  margin: 77px 0px 13px 0px;
+  box-sizing: border-box;
+  padding: 0px 32px;
+  font-size: 18px;
+  border-bottom: 1px solid ${color.middleBar};
   > div {
     display: flex;
     align-items: center;
     > p {
-      margin-right: 13px;
+      margin-left: 15px;
+    }
+    > input {
+      text-align: center;
+      font-size: 18px;
     }
   }
-`;
-export const AllGradeBtn = styled.div`
-  width: 22px;
-  height: 22px;
-  font-size: 17px;
-  border: 1px solid ${color.disable};
-  border-radius: 4px;
-  background-color: white;
-  text-align: center;
-  box-sizing: border-box;
-  padding-top: 1px;
-  cursor: pointer;
-  margin-left: 4px;
+  ${({ border }) => css`
+    border-right: ${border ? `1px solid ${color.middleBar}` : 'none'};
+  `}
 `;
 
-// GradeTable
-export const GradeTable = styled.table`
-  width: 1220px;
-  > tr,
-  td {
-    border: 1px solid black;
-    text-align: center;
-    font-size: 20px;
-  }
-  .bg {
-    background-color: ${color.light};
-    border: none;
-  }
-  > tr {
-    border: none;
-    > td:first-child {
-      border: none;
-    }
+export const GradeColumn = styled.div`
+  display: flex;
+`;
+
+export const GradeTableTd = styled(TableTd)`
+  width: 212px;
+  height: 75px;
+  justify-content: center;
+  > p:last-child {
+    margin: 0px;
   }
 `;
-export const FristLine = styled.td`
-  width: 160px;
-  height: 74px;
-  background-color: ${color.light};
-`;
-export const CheckBox = styled.div`
+
+export const CheckBtnBox = styled.div`
   width: 19px;
   height: 19px;
-  border: 1px solid black;
+  border: 1px solid #000000;
   border-radius: 4px;
-  margin: 0 auto;
+  box-sizing: border-box;
+  padding: 3px;
 `;
-export const CheckedBox = styled.div`
+
+export const CheckedBtnBox = styled.div`
   width: 11px;
   height: 11px;
   background-color: ${color.main};
   border-radius: 2px;
-  margin: 4px 0px 0px 4px;
 `;
-export const GradeBtn = styled.p`
-  cursor: pointer;
+
+export const GradeTr = styled.div`
+  width: 1220px;
+  height: 145px;
+  display: flex;
+  > div > div {
+    display: flex;
+  }
+`;
+
+export const GradeTd = styled.div<{
+  width: number;
+}>`
+  font-size: 20px;
+  box-sizing: border-box;
+  padding: 30px 0px;
+  height: 80px;
+  text-align: center;
+  background-color: ${color.light};
+  ${({ width }) => css`
+    width: ${width}px;
+  `}
+`;
+
+export const SemesterTd = styled(GradeTd)`
   font-size: 18px;
+  height: 65px;
+  padding: 24px 0px;
+`;
+
+export const ResetGrade = styled.div`
+  width: 1220px;
+  height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+  > div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const ResetTitle = styled.p`
+  font-size: 16px;
+  margin-right: 11px;
+`;
+
+export const GradeBtn = styled.div<{
+  isClick: boolean;
+}>`
+  width: 22px;
+  height: 22px;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 17px;
+  margin-left: 4px;
+  box-sizing: border-box;
+  padding: 2px 0px;
+  cursor: pointer;
+  ${({ isClick }) => css`
+    border: 1px solid ${isClick ? color.main : color.disable};
+    color: ${isClick ? color.main : '#000000'};
+  `}
+`;
+
+export const ScoreBtn = styled.p`
+  font-size: 18px;
+  margin-right: 6px;
+  cursor: pointer;
+  :hover {
+    color: ${color.main};
+    font-weight: 900;
+  }
 `;
