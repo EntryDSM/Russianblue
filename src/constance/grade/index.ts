ca7a74ff691fae4b6cd3c90ab7cd679564d2a750
hyejun <hyejun0608@naver.com>
2021-06-08 17:15:43 +0900

    [ADD] grade constance

---
+++ src/constance/grade/index.ts
@@ -0,0 +1,17 @@
+export const SCHOOL = '대덕소프트웨어마이스터고등학교';
+export const ENTERGRADE = '2022년 지원자 성적 입력';
+export const GRADE = [
+  { grade: 'A', id: 'a' },
+  { grade: 'B', id: 'b' },
+  { grade: 'C', id: 'c' },
+  { grade: 'D', id: 'd' },
+  { grade: 'E', id: 'e' },
+  { grade: '0', id: '0' },
+];
+export const GRADEANDSEMESTER = [
+  { id: 11, grade: 1, semester: 1 },
+  { id: 12, grade: 1, semester: 2 },
+  { id: 21, grade: 2, semester: 1 },
+  { id: 22, grade: 2, semester: 2 },
+  { id: 31, grade: 3, semester: 1 },
+];
