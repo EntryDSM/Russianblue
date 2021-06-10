ea98395179f5e2b170cffbd2ae2af8133ef71adc
hyejun <hyejun0608@naver.com>
2021-06-10 14:55:59 +0900

    [FIX] action 수정

--- src/modules/redux/action/grade/interface/index.ts
+++ src/modules/redux/action/grade/interface/index.ts
@@ -1,11 +1,25 @@
 export const INPUT = 'grade/INPUT' as const;
 export const GRADE = 'grade/GRADE' as const;
 export type GradeType = {
-  korean: { 11: string; 12: string; 21: string; 22: string; 31: string };
-  social: { 11: string; 12: string; 21: string; 22: string; 31: string };
-  history: { 11: string; 12: string; 21: string; 22: string; 31: string };
-  math: { 11: string; 12: string; 21: string; 22: string; 31: string };
-  science: { 11: string; 12: string; 21: string; 22: string; 31: string };
-  technical: { 11: string; 12: string; 21: string; 22: string; 31: string };
-  english: { 11: string; 12: string; 21: string; 22: string; 31: string };
+  korean: string;
+  social: string;
+  history: string;
+  math: string;
+  science: string;
+  technical: string;
+  english: string;
+  isCheck: {
+    freshmanFirst: boolean;
+    freshmanSecond: boolean;
+    sophomoreFirst: boolean;
+    sophomoreSecond: boolean;
+    seniorFirst: boolean;
+  };
+};
+export type semesterType = {
+  freshmanFirst: boolean;
+  freshmanSecond: boolean;
+  sophomoreFirst: boolean;
+  sophomoreSecond: boolean;
+  seniorFirst: boolean;
 };
