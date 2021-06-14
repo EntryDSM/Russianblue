31a15d0a1a820619fbaa7c089004dc563b510551
ohjunsang <pandati@dsm.hs.kr>
2021-06-14 15:17:16 +0900

    [FIX] column에 추가된 것 추가

--- src/components/signup/form/column/PhoneNumberColumn.tsx
+++ src/components/signup/form/column/PhoneNumberColumn.tsx
@@ -5,22 +5,43 @@ import Button from '../../../default/button';
 
 interface Props {
   disable: boolean;
+  isSendVertifyCode: boolean;
   setPhoneNumber: (payload: string) => void;
+  sendVertifyCode: (phoneNumber: string) => void;
+  phoneNumber: string;
 }
 
-const PhoneNumberColumn: FC<Props> = ({ disable, setPhoneNumber }) => {
+const PhoneNumberColumn: FC<Props> = ({
+  disable,
+  setPhoneNumber,
+  isSendVertifyCode,
+  sendVertifyCode,
+  phoneNumber,
+}) => {
   const phoneNumberChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPhoneNumber(e.target.value);
   };
+  const sendVertifyCodeButtonClickHandler = () => {
+    console.log('send');
+    sendVertifyCode(phoneNumber);
+  };
   return (
-    <SignUpColumn text='전화번호'>
+    <SignUpColumn
+      text='전화번호'
+      description={true ? '해당 전화번호로 인증번호를 보냈습니다.' : ''}
+    >
       <Input
         width={308}
         disable={disable}
         margin='0px 7px 0px 0px'
         inputChangeHandler={phoneNumberChangeHandler}
       />
-      <Button width={78} disable={disable} margin='0px 0px 0px 7px'>
+      <Button
+        width={78}
+        disable={disable}
+        margin='0px 0px 0px 7px'
+        onClick={sendVertifyCodeButtonClickHandler}
+      >
         전송
       </Button>
     </SignUpColumn>
