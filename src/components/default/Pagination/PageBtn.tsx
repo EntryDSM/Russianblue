c90576934f265d04a030b1fc914a8145101b0b45
hyejun <hyejun0608@naver.com>
2021-06-20 21:58:22 +0900

    [FIX] Pagination

--- src/components/default/Pagination/PageBtn.tsx
+++ src/components/default/Pagination/PageBtn.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import * as S from './style';
 import {
   left_arrow,
@@ -6,21 +6,69 @@ import {
   right_arrow,
   right_arrow_disabled,
 } from '../../../assets/default';
+import { useHistory } from 'react-router-dom';
 
 interface Props {
+  prevPagePath?: string;
+  nextPagePath?: string;
   content: String;
   disabled?: boolean;
 }
 
-const PageBtn: FC<Props> = ({ content, disabled }) => {
+const PageBtn: FC<Props> = ({ content, disabled, prevPagePath, nextPagePath }) => {
+  const history = useHistory();
+
+  const prevBtnClickHandler = () => {
+    history.push(prevPagePath);
+  };
+
+  const nextBtnClickHandler = () => {
+    history.push(nextPagePath);
+  };
+
+  const prevBtn = useMemo(() => {
+    if (content === '이전') {
+      if (disabled)
+        return (
+          <S.PageBtn disabled={disabled}>
+            <img src={left_arrow_disabled} />
+            <div>{content}</div>
+          </S.PageBtn>
+        );
+      else
+        return (
+          <S.PageBtn disabled={disabled} onClick={prevBtnClickHandler}>
+            <img src={left_arrow} />
+            <div>{content}</div>
+          </S.PageBtn>
+        );
+    } else return;
+  }, [disabled]);
+
+  const nextBtn = useMemo(() => {
+    if (content === '다음') {
+      if (disabled)
+        return (
+          <S.PageBtn disabled={disabled}>
+            <div>{content}</div>
+            <img src={right_arrow_disabled} />
+          </S.PageBtn>
+        );
+      else
+        return (
+          <S.PageBtn disabled={disabled} onClick={nextBtnClickHandler}>
+            <div>{content}</div>
+            <img src={right_arrow} />
+          </S.PageBtn>
+        );
+    } else return;
+  }, [disabled]);
+
   return (
-    <S.PageBtn disabled={disabled}>
-      {content === '이전' && disabled && <img src={left_arrow_disabled} />}
-      {content === '이전' && !disabled && <img src={left_arrow} />}
-      <div>{content}</div>
-      {content === '다음' && disabled && <img src={right_arrow_disabled} />}
-      {content === '다음' && !disabled && <img src={right_arrow} />}
-    </S.PageBtn>
+    <>
+      {prevBtn}
+      {nextBtn}
+    </>
   );
 };
 
