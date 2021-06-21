47a205d0a37ae619858ba75be31efb90b2e45ad4
ohjunsang <pandati@dsm.hs.kr>
2021-06-21 15:08:10 +0900

    [FIX] accesstoken 사용 방식 변경

--- src/util/saga/createRequestSaga.ts
+++ src/util/saga/createRequestSaga.ts
@@ -1,23 +1,21 @@
-import { call, put, select } from 'redux-saga/effects';
-import { reducerType } from 'src/modules/redux/reducer';
+import { call, put } from 'redux-saga/effects';
 
 export default function createRequestSaga(type: any, request: any) {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
   return function* (action: any) {
-    console.log(type);
-    const accessToken = yield select((rootReducer: reducerType) => rootReducer.auth.accessToken);
+    const accessToken = localStorage.getItem('access_token');
     try {
       const response = yield call(request, accessToken, action.payload);
       yield put({
         type: SUCCESS,
-        payload: response.data,
+        payload: response ? response.data : null,
       });
     } catch (e) {
       if (e.response?.data) {
         yield put({
           type: FAILURE,
-          payload: e.response.data,
+          payload: { ...e.response.data, type },
         });
       } else {
         yield put({
