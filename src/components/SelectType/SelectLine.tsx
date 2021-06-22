8f36da32f441daca5b516aa096cc84fa22142e95
hyejun <hyejun0608@naver.com>
2021-06-22 22:19:26 +0900

    [FIX] redux 상태들 props로 전달

--- src/components/SelectType/SelectLine.tsx
+++ src/components/SelectType/SelectLine.tsx
@@ -9,12 +9,17 @@ import {
 } from './column';
 
 interface Props {
+  application_type: string;
   socialType: string;
+  is_daejeon: boolean;
+  educational_status: string;
+  graduated_at: string;
   graduationMonth: number;
   graduationYear: number;
+  application_remark: string;
   setType: (payload: string) => void;
   setSocialType: (payload: string) => void;
-  setArea: (payload: string) => void;
+  setArea: (payload: boolean) => void;
   setGraduation: (payload: string) => void;
   setGraduationYear: (payload: number) => void;
   setGraduationMonth: (payload: number) => void;
@@ -22,9 +27,14 @@ interface Props {
 }
 
 const SelectLine: FC<Props> = ({
+  application_type,
   socialType,
+  is_daejeon,
+  educational_status,
+  graduated_at,
   graduationMonth,
   graduationYear,
+  application_remark,
   setType,
   setSocialType,
   setArea,
@@ -36,17 +46,30 @@ const SelectLine: FC<Props> = ({
   const [isProspective, setIsProspective] = useState(false);
   return (
     <S.SelectLine>
-      <ChooseType socialType={socialType} setType={setType} setSocialType={setSocialType} />
-      <ChooseRegion setArea={setArea} />
-      <ChooseGraduation setGraduation={setGraduation} setIsProspective={setIsProspective} />
+      <ChooseType
+        socialType={socialType}
+        setType={setType}
+        setSocialType={setSocialType}
+        application_type={application_type}
+        application_remark={application_remark}
+        setRemark={setRemark}
+      />
+      <ChooseRegion setArea={setArea} is_daejeon={is_daejeon} />
+      <ChooseGraduation
+        setGraduation={setGraduation}
+        setIsProspective={setIsProspective}
+        educational_status={educational_status}
+      />
       <ChooseGraduationDate
+        graduated_at={graduated_at}
+        educational_status={educational_status}
         graduationMonth={graduationMonth}
         graduationYear={graduationYear}
         setGraduationMonth={setGraduationMonth}
         setGraduationYear={setGraduationYear}
         isProspective={isProspective}
       />
-      <ChooseRemark setRemark={setRemark} />
+      <ChooseRemark setRemark={setRemark} application_remark={application_remark} />
     </S.SelectLine>
   );
 };
