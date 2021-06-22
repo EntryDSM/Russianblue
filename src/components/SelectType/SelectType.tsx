9973f520709357051bf244eb226771846b9c5cfc
hyejun <hyejun0608@naver.com>
2021-06-22 22:17:17 +0900

    [FIX] 페이지네이션 코드 구축

--- src/components/SelectType/SelectType.tsx
+++ src/components/SelectType/SelectType.tsx
@@ -1,34 +1,66 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import * as S from './style';
 import { SCHOOL, SELECTTYPE } from '../../constance/SelectType';
 import Pagination from '../default/Pagination';
 import SelectLine from './SelectLine';
 
 interface Props {
+  application_type: string;
   socialType: string;
+  is_daejeon: boolean;
+  educational_status: string;
+  graduated_at: string;
   graduationMonth: number;
   graduationYear: number;
+  application_remark: string;
   setType: (payload: string) => void;
   setSocialType: (payload: string) => void;
-  setArea: (payload: string) => void;
+  setArea: (payload: boolean) => void;
   setGraduation: (payload: string) => void;
   setGraduationYear: (payload: number) => void;
   setGraduationMonth: (payload: number) => void;
   setRemark: (payload: string) => void;
+  selectType: (payload: {
+    educational_status: string;
+    application_type: string;
+    is_daejeon: boolean;
+    application_remark: string;
+    graduated_at: string;
+  }) => void;
 }
 
 const SelectType: FC<Props> = ({
+  application_type,
   socialType,
+  is_daejeon,
+  educational_status,
+  graduated_at,
   graduationMonth,
   graduationYear,
+  application_remark,
   setType,
   setSocialType,
   setArea,
+  selectType,
   setGraduation,
   setGraduationMonth,
   setGraduationYear,
   setRemark,
 }) => {
+  const pagination = useMemo(() => {
+    if (
+      application_type &&
+      is_daejeon !== undefined &&
+      educational_status &&
+      graduationYear &&
+      graduationMonth
+    ) {
+      return <Pagination prevPagePath={'/'} nextPagePath={'/information'} isNextPage />;
+    } else {
+      return <Pagination prevPagePath={'/'} />;
+    }
+  }, [application_type, is_daejeon, educational_status, graduationYear, graduationMonth]);
+
   return (
     <S.SelectType>
       <div>
@@ -36,9 +68,14 @@ const SelectType: FC<Props> = ({
         <S.Title>{SELECTTYPE}</S.Title>
       </div>
       <SelectLine
+        application_type={application_type}
         socialType={socialType}
+        is_daejeon={is_daejeon}
+        educational_status={educational_status}
+        graduated_at={graduated_at}
         graduationMonth={graduationMonth}
         graduationYear={graduationYear}
+        application_remark={application_remark}
         setType={setType}
         setSocialType={setSocialType}
         setArea={setArea}
@@ -47,7 +84,7 @@ const SelectType: FC<Props> = ({
         setGraduationYear={setGraduationYear}
         setRemark={setRemark}
       />
-      <Pagination />
+      {pagination}
     </S.SelectType>
   );
 };
