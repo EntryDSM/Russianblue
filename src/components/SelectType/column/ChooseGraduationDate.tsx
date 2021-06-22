a597a9ce3e01094bddcaa7b9d7fa06e9fb0e604c
hyejun <hyejun0608@naver.com>
2021-06-22 22:23:57 +0900

    [FIX] 검정고시일 때 합격연월을 띄워주고 설명을 삭제시킴

--- src/components/SelectType/column/ChooseGraduationDate.tsx
+++ src/components/SelectType/column/ChooseGraduationDate.tsx
@@ -1,10 +1,12 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useMemo } from 'react';
 import * as S from '../style';
 import { YearSelect, MonthSelect } from '../Select';
 import { GRADUATION_DATE_EXPLAIN } from '../../../constance/SelectType';
 
 interface Props {
+  graduated_at: string;
   isProspective: boolean;
+  educational_status: string;
   graduationMonth: number;
   graduationYear: number;
   setGraduationYear: (payload: number) => void;
@@ -13,16 +15,51 @@ interface Props {
 
 const ChooseGraduationDate: FC<Props> = ({
   isProspective,
+  educational_status,
   graduationMonth,
   graduationYear,
   setGraduationMonth,
   setGraduationYear,
+  graduated_at,
 }) => {
+  useEffect(() => {
+    if (graduated_at) {
+      const graduatedYear = graduated_at.slice(0, 4);
+      const graduatedMonth = graduated_at.slice(4);
+      setGraduationYear(Number(graduatedYear));
+      setGraduationMonth(Number(graduatedMonth));
+    }
+  }, [graduated_at]);
+
+  const graduationDateTitle = useMemo(() => {
+    if (educational_status === 'QUALIFICATION_EXAM')
+      return (
+        <S.LineTitle>
+          <span>*</span>합격 연월
+        </S.LineTitle>
+      );
+    else
+      return (
+        <S.LineTitle>
+          <span>*</span>졸업 연월
+        </S.LineTitle>
+      );
+  }, [educational_status]);
+
+  const explain = useMemo(() => {
+    if (educational_status === 'QUALIFICATION_EXAM') return;
+    else
+      return (
+        <S.Explain>
+          <span>*</span>
+          {GRADUATION_DATE_EXPLAIN}
+        </S.Explain>
+      );
+  }, [educational_status]);
+
   return (
     <S.Line>
-      <S.LineTitle>
-        <span>*</span>졸업 연월
-      </S.LineTitle>
+      {graduationDateTitle}
       <YearSelect
         graduationYear={graduationYear}
         setGraduationYear={setGraduationYear}
@@ -31,10 +68,7 @@ const ChooseGraduationDate: FC<Props> = ({
       <S.Date>년</S.Date>
       <MonthSelect graduationMonth={graduationMonth} setGraduationMonth={setGraduationMonth} />
       <S.Date>월</S.Date>
-      <S.Explain>
-        <span>*</span>
-        {GRADUATION_DATE_EXPLAIN}
-      </S.Explain>
+      {explain}
     </S.Line>
   );
 };
