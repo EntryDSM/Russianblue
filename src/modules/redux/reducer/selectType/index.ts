a2d9b559763db4d922c8233e2a38be6a59663824
hyejun <hyejun0608@naver.com>
2021-06-22 22:15:20 +0900

    [FIX] 서버 연동 액션 & 자동 서버 연동 상태 제작

--- src/modules/redux/reducer/selectType/index.ts
+++ src/modules/redux/reducer/selectType/index.ts
@@ -7,17 +7,29 @@ import {
   GRADUATION_MONTH,
   GRADUATION_YEAR,
   REMARK,
+  SELECTTYPE,
+  SELECTTYPE_SUCCESS,
+  SELECTTYPE_FAILURE,
+  GET_SELECTTYPE_FAILURE,
+  GET_SELECTTYPE_SUCCESS,
+  AUTOSAVE_SELECTTYPE,
+  AUTOSAVE_SELECTTYPE_SUCCESS,
+  AUTOSAVE_SELECTTYPE_FAILURE,
 } from '../../action/selectType/interface';
 import SelectTypeState from './interface';
 
 const initState: SelectTypeState = {
-  type: '',
+  application_type: '',
   socialType: '사회통합전형',
-  area: undefined,
-  graduation: '',
+  is_daejeon: undefined,
+  educational_status: '',
   graduationMonth: 1,
   graduationYear: 2022,
-  remark: '',
+  graduated_at: '',
+  application_remark: null,
+  error: null,
+  isSuccessSaveSelectType: undefined,
+  isSuccessGetSaveSelectType: undefined,
 };
 
 const SelectTypeReducer = (
@@ -28,7 +40,7 @@ const SelectTypeReducer = (
     case TYPE:
       return {
         ...state,
-        type: action.payload,
+        application_type: action.payload,
       };
     case SOCIALTYPE:
       return {
@@ -38,12 +50,12 @@ const SelectTypeReducer = (
     case AREA:
       return {
         ...state,
-        area: action.payload,
+        is_daejeon: action.payload,
       };
     case GRADUATION:
       return {
         ...state,
-        graduation: action.payload,
+        educational_status: action.payload,
       };
     case GRADUATION_MONTH:
       return {
@@ -58,7 +70,63 @@ const SelectTypeReducer = (
     case REMARK:
       return {
         ...state,
-        remark: action.payload,
+        application_remark: action.payload,
+      };
+    case SELECTTYPE:
+      return {
+        ...state,
+        isSuccessSaveSelectType: undefined,
+        application_remark: action.payload.application_remark,
+        is_daejeon: action.payload.is_daejeon,
+        application_type: action.payload.application_type,
+        educational_status: action.payload.educational_status,
+        graduated_at: action.payload.graduated_at,
+      };
+    case SELECTTYPE_SUCCESS:
+      return {
+        ...state,
+        isSuccessSaveSelectType: true,
+      };
+    case SELECTTYPE_FAILURE:
+      return {
+        ...state,
+        isSuccessSaveSelectType: false,
+        error: action.payload,
+      };
+    case AUTOSAVE_SELECTTYPE:
+      return {
+        ...state,
+        isSuccessSaveSelectType: undefined,
+        application_remark: action.payload.application_remark,
+        is_daejeon: action.payload.is_daejeon,
+        application_type: action.payload.application_type,
+        educational_status: action.payload.educational_status,
+        graduated_at: action.payload.graduated_at,
+      };
+    case AUTOSAVE_SELECTTYPE_SUCCESS:
+      return {
+        ...state,
+      };
+    case AUTOSAVE_SELECTTYPE_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_SELECTTYPE_SUCCESS:
+      return {
+        ...state,
+        isSuccessGetSaveSelectType: true,
+        application_remark: action.payload.application_remark,
+        is_daejeon: action.payload.is_daejeon,
+        application_type: action.payload.application_type,
+        educational_status: action.payload.educational_status,
+        graduated_at: action.payload.graduated_at,
+      };
+    case GET_SELECTTYPE_FAILURE:
+      return {
+        ...state,
+        isSuccessGetSaveSelectType: false,
+        error: action.payload,
       };
     default:
       return state;
