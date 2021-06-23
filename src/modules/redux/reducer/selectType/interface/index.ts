d14493a36f4e9f8ebb711d73d0225b72ecc83c61
hyejun <hyejun0608@naver.com>
2021-06-23 17:16:15 +0900

    [FIX] GetSave -> Get 으로 네이밍 수정

--- src/modules/redux/reducer/selectType/interface/index.ts
+++ src/modules/redux/reducer/selectType/interface/index.ts
@@ -11,7 +11,7 @@ interface SelectTypeState {
   error: error;
   graduated_at: string;
   isSuccessSaveSelectType: boolean | undefined;
-  isSuccessGetSaveSelectType: boolean | undefined;
+  isSuccessGetSelectType: boolean | undefined;
 }
 
 export default SelectTypeState;
