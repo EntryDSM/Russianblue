3566d02df2ccccfd69bd2f24fdf322ac2f952d4e
hyejun <hyejun0608@naver.com>
2021-06-24 20:58:29 +0900

    [FIX] useInformation에 서버 요청하는 hook 제작

--- src/util/hooks/information/useInformation.tsx
+++ src/util/hooks/information/useInformation.tsx
@@ -7,6 +7,9 @@ import {
   setBirthYear,
   setImageUrl,
   setImageFile,
+  information,
+  autoSaveInformation,
+  informationImage,
 } from '../../../modules/redux/action/information';
 import { useSelectState } from '../default';
 
@@ -32,8 +35,49 @@ const useInformation = () => {
     setImageUrl: (payload: string) => {
       dispatch(setImageUrl(payload));
     },
-    setImageFile: (payload: string) => {
+    setImageFile: (payload: File) => {
       dispatch(setImageFile(payload));
+    },
+    information: (payload: {
+      name: string;
+      gender: string;
+      birthDay: string;
+      schoolName: string;
+      schoolPhoneNumber: string;
+      parentName: string;
+      parentPhoneNumber: string;
+      phoneNumber: string;
+      homePhoneNumber: string;
+      baseAddress: string;
+      detailAddress: string;
+      zipcode: string;
+      imageUrl: string;
+      grade: string;
+      isGraduated: boolean;
+    }) => {
+      dispatch(information(payload));
+    },
+    autoSaveInformation: (payload: {
+      name: string;
+      gender: string;
+      birthDay: string;
+      schoolName: string;
+      schoolPhoneNumber: string;
+      parentName: string;
+      parentPhoneNumber: string;
+      phoneNumber: string;
+      homePhoneNumber: string;
+      baseAddress: string;
+      detailAddress: string;
+      zipcode: string;
+      imageUrl: string;
+      grade: string;
+      isGraduated: boolean;
+    }) => {
+      dispatch(autoSaveInformation(payload));
+    },
+    informationImage: (payload: File) => {
+      dispatch(informationImage(payload));
     },
   };
   return { state, setState };
