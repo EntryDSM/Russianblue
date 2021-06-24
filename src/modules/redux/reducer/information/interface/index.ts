89336bfa9a800fe8b663fec988cbe1f348b222d7
hyejun <hyejun0608@naver.com>
2021-06-24 15:23:28 +0900

    [FIX] information 상태들의 타입 지정

--- src/modules/redux/reducer/information/interface/index.ts
+++ src/modules/redux/reducer/information/interface/index.ts
@@ -1,12 +1,32 @@
+import { error } from '../../../../../models/error';
+
 interface InformationState {
   name: string;
-  value: string;
   gender: string;
+  birthDay: string;
   birthYear: number;
   birthMonth: number;
   birthDate: number;
+  schoolName: string;
+  schoolPhoneNumber: string;
+  parentName: string;
+  parentPhoneNumber: string;
+  phoneNumber: string;
+  homePhoneNumber: string;
+  zipCode: string;
+  fullAddress: string;
+  baseAddress: string;
+  detailAddress: string;
+  grade: string;
+  stdGrade: string;
+  stdClass: string;
+  stdNumber: string;
   imageUrl: string;
-  imageFile: File;
+  imageFile: String;
+  isGraduated: boolean;
+  isSuccessSaveInformation: boolean | undefined;
+  isSuccessGetInformation: boolean | undefined;
+  error: error;
 }
 
 export default InformationState;
