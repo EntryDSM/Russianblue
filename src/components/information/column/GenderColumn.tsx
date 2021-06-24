5dfd2e4ebff638409cb48e75619f7d6e496ebd26
hyejun <hyejun0608@naver.com>
2021-06-24 20:55:23 +0900

    [FIX] 배열 map 함수를 useMemo로 빼줌 & gender값이 바뀔 때 마다 체크박스가 알맞게 채워짐

--- src/components/information/column/GenderColumn.tsx
+++ src/components/information/column/GenderColumn.tsx
@@ -1,39 +1,49 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 import * as S from '../style';
 import { GENDER } from '../../../constance/information';
 
 interface Props {
   width: number;
+  gender: string;
   setGender: (payload: string) => void;
 }
 
-const GenderColumn: FC<Props> = ({ setGender, width }) => {
+const GenderColumn: FC<Props> = ({ setGender, width, gender }) => {
   const [genderBtnCheck, setGenderBtnCheck] = useState({ male: false, female: false });
   const onCheckBtnClick = e => {
     let dataId = e.target.dataset.id;
     if (dataId === 'male') {
-      setGender('남자');
-      setGenderBtnCheck({ male: true, female: false });
+      setGender('MALE');
     } else {
-      setGender('여자');
-      setGenderBtnCheck({ male: false, female: true });
+      setGender('FEMALE');
     }
   };
+
+  useEffect(() => {
+    if (gender === 'MALE') setGenderBtnCheck({ male: true, female: false });
+    else if (gender === 'FEMALE') setGenderBtnCheck({ male: false, female: true });
+    console.log(gender);
+  }, [gender]);
+
+  const showGenderCheck = useMemo(() => {
+    return GENDER.map(data => {
+      return (
+        <S.SelectBox key={data.id}>
+          <S.CheckCircle onClick={onCheckBtnClick} data-id={data.id}>
+            {genderBtnCheck[data.id] && <S.CheckedCircle />}
+          </S.CheckCircle>
+          <p>{data.content}</p>
+        </S.SelectBox>
+      );
+    });
+  }, [genderBtnCheck]);
+
   return (
     <S.InformationLine width={width}>
       <S.InformationLineTitle>
         <span>*</span>성별
       </S.InformationLineTitle>
-      {GENDER.map(data => {
-        return (
-          <S.SelectBox key={data.id}>
-            <S.CheckCircle onClick={onCheckBtnClick} data-id={data.id}>
-              {genderBtnCheck[data.id] && <S.CheckedCircle />}
-            </S.CheckCircle>
-            <p>{data.content}</p>
-          </S.SelectBox>
-        );
-      })}
+      {showGenderCheck}
     </S.InformationLine>
   );
 };
