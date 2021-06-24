f6ff92898119c3da0dcc56127fb5a7664a989b33
hyejun <hyejun0608@naver.com>
2021-06-24 15:22:37 +0900

    [FIX] 서버 통신을 위한 reducer 추가

--- src/modules/redux/reducer/information/index.ts
+++ src/modules/redux/reducer/information/index.ts
@@ -7,18 +7,45 @@ import {
   BIRTHYEAR,
   IMAGEURL,
   IMAGEFILE,
+  INFORMATION,
+  INFORMATION_SUCCESS,
+  INFORMATION_FAILURE,
+  AUTOSAVE_INFORMATION,
+  AUTOSAVE_INFORMATION_SUCCESS,
+  AUTOSAVE_INFORMATION_FAILURE,
+  GET_INFORMATION,
+  GET_INFORMATION_SUCCESS,
+  GET_INFORMATION_FAILURE,
 } from '../../action/information/interface';
 import InformationState from './interface';
 
 const initState: InformationState = {
   name: '',
-  value: '',
   gender: '',
+  birthDay: '',
   birthYear: 2006,
   birthMonth: 1,
   birthDate: 1,
+  schoolName: '',
+  schoolPhoneNumber: '',
+  parentName: '',
+  parentPhoneNumber: '',
+  phoneNumber: '',
+  homePhoneNumber: '',
+  zipCode: '',
+  fullAddress: '',
+  baseAddress: '',
+  detailAddress: '',
+  grade: '',
+  stdGrade: '',
+  stdClass: '',
+  stdNumber: '',
   imageUrl: '',
-  imageFile: null,
+  imageFile: '',
+  isGraduated: false,
+  error: null,
+  isSuccessGetInformation: undefined,
+  isSuccessSaveInformation: undefined,
 };
 
 const informationReducer = (
@@ -60,6 +87,91 @@ const informationReducer = (
       return {
         ...state,
         imageFile: action.payload,
+      };
+    case INFORMATION:
+      return {
+        ...state,
+        isSuccessSaveInformation: undefined,
+        name: action.payload.name,
+        gender: action.payload.grade,
+        birthDay: action.payload.birthDay,
+        schoolName: action.payload.schoolName,
+        schoolPhoneNumber: action.payload.schoolPhoneNumber,
+        parentName: action.payload.parentName,
+        parentPhoneNumber: action.payload.parentPhoneNumber,
+        phoneNumber: action.payload.phoneNumber,
+        homePhoneNumber: action.payload.homePhoneNumber,
+        baseAddress: action.payload.baseAddress,
+        detailAddress: action.payload.detailAddress,
+        zipCode: action.payload.zipcode,
+        imageFile: action.payload.imageFile,
+        grade: action.payload.grade,
+        isGraduated: action.payload.isGraduated,
+      };
+    case INFORMATION_SUCCESS:
+      return {
+        ...state,
+        isSuccessSaveInformation: true,
+      };
+    case INFORMATION_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case AUTOSAVE_INFORMATION:
+      return {
+        ...state,
+        isSuccessSaveInformation: undefined,
+        name: action.payload.name,
+        gender: action.payload.grade,
+        birthDay: action.payload.birthDay,
+        schoolName: action.payload.schoolName,
+        schoolPhoneNumber: action.payload.schoolPhoneNumber,
+        parentName: action.payload.parentName,
+        parentPhoneNumber: action.payload.parentPhoneNumber,
+        phoneNumber: action.payload.phoneNumber,
+        homePhoneNumber: action.payload.homePhoneNumber,
+        baseAddress: action.payload.baseAddress,
+        detailAddress: action.payload.detailAddress,
+        zipCode: action.payload.zipcode,
+        imageFile: action.payload.imageFile,
+        grade: action.payload.grade,
+        isGraduated: action.payload.isGraduated,
+      };
+    case AUTOSAVE_INFORMATION_SUCCESS:
+      return {
+        ...state,
+      };
+    case AUTOSAVE_INFORMATION_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case GET_INFORMATION_SUCCESS:
+      return {
+        ...state,
+        isSuccessSaveInformation: true,
+        name: action.payload.name,
+        gender: action.payload.sex,
+        birthDay: action.payload.birthday,
+        schoolName: action.payload.school_code,
+        schoolPhoneNumber: action.payload.school_tel,
+        parentName: action.payload.parent_name,
+        parentPhoneNumber: action.payload.parent_tel,
+        phoneNumber: action.payload.telephone_number,
+        homePhoneNumber: action.payload.home_tel,
+        baseAddress: action.payload.address,
+        detailAddress: action.payload.detail_address,
+        zipCode: action.payload.post_code,
+        imageFile: action.payload.photo_file_name,
+        grade: action.payload.student_number,
+        isGraduated: action.payload.is_graduated,
+      };
+    case GET_INFORMATION_FAILURE:
+      return {
+        ...state,
+        isSuccessGetInformation: false,
+        error: action.payload,
       };
     default:
       return state;
