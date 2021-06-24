952d9284e3c711f1c7ce1dd427b4aa2a47c0d288
hyejun <hyejun0608@naver.com>
2021-06-24 15:30:49 +0900

    [FIX] 졸업구분 데이터 받아옴

--- src/containers/information/InformationContainer.tsx
+++ src/containers/information/InformationContainer.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useInformation } from '../../util/hooks/information';
 import Information from '../../components/information';
+import { useDispatch } from 'react-redux';
+import { GET_SELECTTYPE } from '../../modules/redux/action/selectType/interface';
 
 const InformationContainer = () => {
   const { state, setState } = useInformation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: GET_SELECTTYPE });
+  }, []);
   return <Information {...state} {...setState} />;
 };
 
