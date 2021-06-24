eacf83ca03d8794bd73843e120f4bccba2f184c1
hyejun <hyejun0608@naver.com>
2021-06-24 20:58:01 +0900

    [ADD] information api 호출을 위한 hook 생성

---
+++ src/util/api/information/index.ts
@@ -0,0 +1,106 @@
+import uri from '../../../constance/uri';
+import { getRequestWithAccessToken } from '../default';
+
+export const information = async (
+  access_token: string,
+  informationRequest: {
+    name: string;
+    gender: string;
+    birthDay: string;
+    schoolName: string;
+    schoolPhoneNumber: string;
+    parentName: string;
+    parentPhoneNumber: string;
+    phoneNumber: string;
+    homePhoneNumber: string;
+    baseAddress: string;
+    detailAddress: string;
+    zipCode: string;
+    imageFile: string;
+    grade: string;
+    isGraduated: boolean;
+  },
+) => {
+  try {
+    const request = getRequestWithAccessToken(access_token);
+    return await request.patch(uri.information, {
+      name: informationRequest.name,
+      sex: informationRequest.gender,
+      birthday: informationRequest.birthDay,
+      school_code: informationRequest.schoolName,
+      school_tel: informationRequest.schoolPhoneNumber,
+      parent_name: informationRequest.parentName,
+      parent_tel: informationRequest.parentPhoneNumber,
+      telephone_number: informationRequest.phoneNumber,
+      home_tel: informationRequest.homePhoneNumber,
+      address: informationRequest.baseAddress,
+      detail_address: informationRequest.detailAddress,
+      post_code: informationRequest.zipCode,
+      student_number: informationRequest.grade,
+      is_graduated: informationRequest.isGraduated,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const autoSaveInformation = async (
+  access_token: string,
+  informationRequest: {
+    name: string;
+    gender: string;
+    birthDay: string;
+    schoolName: string;
+    schoolPhoneNumber: string;
+    parentName: string;
+    parentPhoneNumber: string;
+    phoneNumber: string;
+    homePhoneNumber: string;
+    baseAddress: string;
+    detailAddress: string;
+    zipCode: string;
+    imageFile: string;
+    grade: string;
+    isGraduated: boolean;
+  },
+) => {
+  try {
+    const request = getRequestWithAccessToken(access_token);
+    return await request.patch(uri.information, {
+      name: informationRequest.name,
+      sex: informationRequest.gender,
+      birthday: informationRequest.birthDay,
+      school_code: informationRequest.schoolName,
+      school_tel: informationRequest.schoolPhoneNumber,
+      parent_name: informationRequest.parentName,
+      parent_tel: informationRequest.parentPhoneNumber,
+      telephone_number: informationRequest.phoneNumber,
+      home_tel: informationRequest.homePhoneNumber,
+      address: informationRequest.baseAddress,
+      detail_address: informationRequest.detailAddress,
+      post_code: informationRequest.zipCode,
+      student_number: informationRequest.grade,
+      is_graduated: informationRequest.isGraduated,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getInformation = async (access_token: string) => {
+  try {
+    const request = getRequestWithAccessToken(access_token);
+    return await request.get(uri.information);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const informationImage = async (access_token: string, informationImageRequest) => {
+  try {
+    const request = getRequestWithAccessToken(access_token);
+    await request.post(uri.informationImage, { file: informationImageRequest });
+  } catch (error) {
+    throw error;
+  }
+};
