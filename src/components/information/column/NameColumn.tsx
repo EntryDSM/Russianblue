c4822d6eab9492ccebddfdb3541d264a417c0d79
hyejun <hyejun0608@naver.com>
2021-06-24 20:54:41 +0900

    [FIX] 서버 요청으로 이름 데이터값이 들어오면 input defaultValue값으로 보여줌

--- src/components/information/column/NameColumn.tsx
+++ src/components/information/column/NameColumn.tsx
@@ -1,25 +1,51 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useMemo } from 'react';
 import * as S from '../style';
 import Input from '../../default/input';
 
 interface Props {
+  name?: string;
+  parentName?: string;
   title: string;
   width: number;
-  name: string;
+  inputName: string;
   setInput: (payload: { name: string; value: string }) => void;
 }
 
-const NameColumn: FC<Props> = ({ title, width, name, setInput }) => {
+const NameColumn: FC<Props> = ({ title, width, inputName, setInput, name, parentName }) => {
   const nameChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput({ name: e.target.name, value: e.target.value });
   };
+
+  const nameInput = useMemo(() => {
+    if (inputName === 'name')
+      return (
+        <Input
+          width={166}
+          height={42}
+          inputChangeHandler={nameChangeHandler}
+          name={inputName}
+          defaultValue={name}
+        />
+      );
+    else if (inputName === 'parentName')
+      return (
+        <Input
+          width={166}
+          height={42}
+          inputChangeHandler={nameChangeHandler}
+          name={inputName}
+          defaultValue={parentName}
+        />
+      );
+  }, [name, parentName]);
+
   return (
     <S.InformationLine width={width}>
       <S.InformationLineTitle>
         <span>*</span>
         {title}
       </S.InformationLineTitle>
-      <Input width={166} height={42} inputChangeHandler={nameChangeHandler} name={name} />
+      {nameInput}
     </S.InformationLine>
   );
 };
