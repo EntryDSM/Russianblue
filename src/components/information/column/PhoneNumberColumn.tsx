9261478bcb7b6beee7ca9a06629fb968aa7c8068
hyejun <hyejun0608@naver.com>
2021-06-28 20:22:52 +0900

    [FIX] 전화번호 값이 있다면 각각 그 값을 보여줌

--- src/components/information/column/PhoneNumberColumn.tsx
+++ src/components/information/column/PhoneNumberColumn.tsx
@@ -1,25 +1,55 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import * as S from '../style';
 import Input from '../../default/input';
 import { EXPLAIN } from '../../../constance/information';
 
 interface Props {
   title: string;
-  name: string;
+  inputName: string;
+  schoolPhoneNumber?: string;
+  parentPhoneNumber?: string;
+  phoneNumber?: string;
+  homePhoneNumber?: string;
   setInput: (payload: { name: string; value: string }) => void;
 }
 
-const PhoneNumberColumn: FC<Props> = ({ title, name, setInput }) => {
+const PhoneNumberColumn: FC<Props> = ({
+  title,
+  inputName,
+  setInput,
+  schoolPhoneNumber,
+  parentPhoneNumber,
+  phoneNumber,
+  homePhoneNumber,
+}) => {
   const phoneNumberChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput({ name: e.target.name, value: e.target.value });
   };
+
+  const phoneNumberInput = useMemo(() => {
+    let inputDefaultValue = '';
+    if (inputName === 'schoolPhoneNumber') inputDefaultValue = schoolPhoneNumber;
+    else if (inputName === 'parentPhoneNumber') inputDefaultValue = parentPhoneNumber;
+    else if (inputName === 'phoneNumber') inputDefaultValue = phoneNumber;
+    else if (inputName === 'homePhoneNumber') inputDefaultValue = homePhoneNumber;
+    return (
+      <Input
+        width={260}
+        height={41}
+        inputChangeHandler={phoneNumberChangeHandler}
+        name={inputName}
+        defaultValue={inputDefaultValue}
+      />
+    );
+  }, [schoolPhoneNumber, parentPhoneNumber, phoneNumber, homePhoneNumber]);
+
   return (
     <S.InformationLine width={1220}>
       <S.InformationLineTitle>
-        <span>{name === 'homePhoneNumber' ? '' : '*'}</span>
+        <span>{inputName === 'homePhoneNumber' ? '' : '*'}</span>
         {title}
       </S.InformationLineTitle>
-      <Input width={260} height={41} inputChangeHandler={phoneNumberChangeHandler} name={name} />
+      {phoneNumberInput}
       <S.Explain>
         <span>*</span>
         {EXPLAIN}
