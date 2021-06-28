2c98eee66d442245666034773bc1b6500a9b682f
hyejun <hyejun0608@naver.com>
2021-06-28 20:23:53 +0900

    [FIX] 학번 값이 있다면 그 값을 각각 학년반번호로 보여줌

--- src/components/information/column/GradeColumn.tsx
+++ src/components/information/column/GradeColumn.tsx
@@ -4,24 +4,37 @@ import Input from '../../default/input';
 import { GRADE } from '../../../constance/information';
 
 interface Props {
+  stdGrade: string;
+  stdNumber: string;
+  stdClass: string;
   setInput: (payload: { name: string; value: string }) => void;
 }
 
-const GradeColumn: FC<Props> = ({ setInput }) => {
+const GradeColumn: FC<Props> = ({ setInput, stdClass, stdGrade, stdNumber }) => {
   const gradeChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput({ name: e.target.name, value: e.target.value });
   };
 
   const grade = useMemo(() => {
+    let gradeDefaultValue = '';
     return GRADE.map(grade => {
+      if (grade.name === 'stdGrade') gradeDefaultValue = stdGrade;
+      else if (grade.name === 'stdClass') gradeDefaultValue = stdClass;
+      else if (grade.name === 'stdNumber') gradeDefaultValue = stdNumber;
       return (
         <>
-          <Input width={63} height={42} inputChangeHandler={gradeChangeHandler} name={grade.name} />
+          <Input
+            width={63}
+            height={42}
+            inputChangeHandler={gradeChangeHandler}
+            defaultValue={gradeDefaultValue}
+            name={grade.name}
+          />
           <S.Unit>{grade.content}</S.Unit>
         </>
       );
     });
-  }, []);
+  }, [stdGrade, stdClass, stdNumber]);
 
   return (
     <S.InformationLine width={860}>
