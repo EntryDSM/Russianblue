e19c9c4df8722d5485327409732a29cae33f706e
hyejun <hyejun0608@naver.com>
2021-06-28 20:19:02 +0900

    [FIX] 학교 검색 api를 redux-saga에 등록

--- src/modules/redux/middleware/information/index.ts
+++ src/modules/redux/middleware/information/index.ts
@@ -4,12 +4,14 @@ import {
   informationImage,
   getInformation,
   autoSaveInformation,
+  searchSchool,
 } from '../../../../util/api/information';
 import {
   AUTOSAVE_INFORMATION,
   GET_INFORMATION,
   INFORMATION,
   INFORMATION_IMAGE,
+  SEARCH_SCHOOL,
 } from '../../action/information/interface';
 import { debounce, takeLatest } from 'redux-saga/effects';
 
@@ -20,11 +22,13 @@ export const autoSaveInformationRequestSaga = createRequestSaga(
   autoSaveInformation,
 );
 export const informationImageRequestSaga = createRequestSaga(INFORMATION_IMAGE, informationImage);
+export const searchSchoolRequestSaga = createRequestSaga(SEARCH_SCHOOL, searchSchool);
 
 function* informationSaga() {
   yield takeLatest(INFORMATION, informationRequestSaga);
   yield takeLatest(GET_INFORMATION, getSelectTypeRequestSaga);
   yield takeLatest(INFORMATION_IMAGE, informationImageRequestSaga);
+  yield takeLatest(SEARCH_SCHOOL, searchSchoolRequestSaga);
   yield debounce(3000, AUTOSAVE_INFORMATION, autoSaveInformationRequestSaga);
 }
 
