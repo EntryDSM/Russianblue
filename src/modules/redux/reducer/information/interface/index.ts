8bd90d17290e7d2635796f63c7b22c4439552396
hyejun <hyejun0608@naver.com>
2021-06-30 17:38:27 +0900

    [FIX] 서버 연동에 필요한 상태 타입 지정

--- src/modules/redux/reducer/information/interface/index.ts
+++ src/modules/redux/reducer/information/interface/index.ts
@@ -4,11 +4,11 @@ import { error } from '../../../../../models/error';
 interface InformationState {
   name: string;
   gender: string;
-  birthDay: string;
   birthYear: number;
   birthMonth: number;
   birthDate: number;
   schoolCode: string;
+  schoolName: string;
   schoolPhoneNumber: string;
   parentName: string;
   parentPhoneNumber: string;
@@ -21,19 +21,20 @@ interface InformationState {
   stdGrade: string;
   stdClass: string;
   stdNumber: string;
+  totalScore: number;
   imageUrl: string;
   imageFile: File;
-  isGraduated: boolean;
-  isSuccessSaveInformationImage: boolean | undefined;
-  isSuccessSaveInformation: boolean | undefined;
-  isSuccessGetInformation: boolean | undefined;
-  isSuccessSaveSearchSchool: boolean | undefined;
-  error: error;
   schoolSearchName: string;
   page: number;
   size: number;
   content: Array<schoolArrayType>;
   totalPages: number;
+  isSuccessSaveInformationImage: boolean | undefined;
+  isSuccessSaveInformation: boolean | undefined;
+  isSuccessGetInformation: boolean | undefined;
+  isSuccessSaveSearchSchool: boolean | undefined;
+  isSuccessSaveGedInformation: boolean | undefined;
+  error: error;
 }
 
 export default InformationState;
