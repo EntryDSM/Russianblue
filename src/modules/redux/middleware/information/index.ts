5fe34602d2a0686e20f09bd79e70af989ecf2e3a
hyejun <hyejun0608@naver.com>
2021-06-30 17:47:01 +0900

    [FIX] 서버 연동을 위한 코드를 redux-saga에 등록

--- src/modules/redux/middleware/information/index.ts
+++ src/modules/redux/middleware/information/index.ts
@@ -5,6 +5,9 @@ import {
   getInformation,
   autoSaveInformation,
   searchSchool,
+  gedInformation,
+  getGedInformation,
+  autoSaveGedInformation,
 } from '../../../../util/api/information';
 import {
   AUTOSAVE_INFORMATION,
@@ -12,23 +15,38 @@ import {
   INFORMATION,
   INFORMATION_IMAGE,
   SEARCH_SCHOOL,
+  GEDINFORMATION,
+  GET_GEDINFORMATION,
+  AUTOSAVE_GEDINFORMATION,
 } from '../../action/information/interface';
 import { debounce, takeLatest } from 'redux-saga/effects';
 
 export const informationRequestSaga = createRequestSaga(INFORMATION, information);
-export const getSelectTypeRequestSaga = createRequestSaga(GET_INFORMATION, getInformation);
+export const getInformationRequestSaga = createRequestSaga(GET_INFORMATION, getInformation);
 export const autoSaveInformationRequestSaga = createRequestSaga(
   AUTOSAVE_INFORMATION,
   autoSaveInformation,
 );
 export const informationImageRequestSaga = createRequestSaga(INFORMATION_IMAGE, informationImage);
 export const searchSchoolRequestSaga = createRequestSaga(SEARCH_SCHOOL, searchSchool);
+export const gedInformationRequestSaga = createRequestSaga(GEDINFORMATION, gedInformation);
+export const getGedInformationRequestSaga = createRequestSaga(
+  GET_GEDINFORMATION,
+  getGedInformation,
+);
+export const autoSaveGedInformationRequestSaga = createRequestSaga(
+  AUTOSAVE_GEDINFORMATION,
+  autoSaveGedInformation,
+);
 
 function* informationSaga() {
   yield takeLatest(INFORMATION, informationRequestSaga);
-  yield takeLatest(GET_INFORMATION, getSelectTypeRequestSaga);
+  yield takeLatest(GET_INFORMATION, getInformationRequestSaga);
   yield takeLatest(INFORMATION_IMAGE, informationImageRequestSaga);
   yield takeLatest(SEARCH_SCHOOL, searchSchoolRequestSaga);
+  yield takeLatest(GEDINFORMATION, gedInformationRequestSaga);
+  yield takeLatest(GET_GEDINFORMATION, getGedInformationRequestSaga);
+  yield debounce(3000, AUTOSAVE_GEDINFORMATION, autoSaveGedInformationRequestSaga);
   yield debounce(3000, AUTOSAVE_INFORMATION, autoSaveInformationRequestSaga);
 }
 
