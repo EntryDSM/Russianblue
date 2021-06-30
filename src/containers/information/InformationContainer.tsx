9e35d201f607df49beaf5aab363736be53bf22b3
hyejun <hyejun0608@naver.com>
2021-06-30 17:52:06 +0900

    [FIX] 검정고시일 때 검정고시 인적사항 받아오는 api 연동

--- src/containers/information/InformationContainer.tsx
+++ src/containers/information/InformationContainer.tsx
@@ -1,18 +1,29 @@
 import React, { useEffect } from 'react';
-import { useInformation } from '../../util/hooks/information';
+import useInformation from '../../util/hooks/information';
 import Information from '../../components/information';
 import { useDispatch } from 'react-redux';
 import { GET_SELECTTYPE } from '../../modules/redux/action/selectType/interface';
-import { GET_INFORMATION } from '../../modules/redux/action/information/interface';
+import {
+  GET_GEDINFORMATION,
+  GET_INFORMATION,
+} from '../../modules/redux/action/information/interface';
+import useSelectType from '../../util/hooks/selectType';
 
 const InformationContainer = () => {
   const { state, setState } = useInformation();
+  const educationalStatus = useSelectType().state.educationalStatus;
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({ type: GET_SELECTTYPE });
-    dispatch({ type: GET_INFORMATION });
   }, []);
+  useEffect(() => {
+    if (educationalStatus === 'QUALIFICATION_EXAM') {
+      dispatch({ type: GET_GEDINFORMATION });
+    } else {
+      dispatch({ type: GET_INFORMATION });
+    }
+  }, [educationalStatus]);
   return <Information {...state} {...setState} />;
 };
 
