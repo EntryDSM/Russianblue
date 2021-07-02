dc7677d9e8621f04bd97cabab334b698a7ffefce
hyejun <hyejun0608@naver.com>
2021-07-02 21:45:59 +0900

    [FIX] 새롭게 서버 요청 코드를 구축

--- src/util/hooks/information/useInformation.tsx
+++ src/util/hooks/information/useInformation.tsx
@@ -1,157 +1,56 @@
 import { useDispatch } from 'react-redux';
-import { addressType, gedInformationType, informationType } from 'src/constance/information';
+import { addressType, searchSchoolQueryType } from 'src/constance/information';
 import {
   setInput,
-  setGender,
+  setSex,
   setBirthDate,
   setBirthMonth,
   setBirthYear,
   setImageUrl,
-  setImageFile,
-  gedInformation,
-  information,
-  autoSaveInformation,
-  informationImage,
-  searchSchool,
   setSchoolCode,
   setAddress,
-  autoSaveGedInformation,
+  userPicture,
+  searchSchool,
+  setSchoolName,
 } from '../../../modules/redux/action/information';
 import { useSelectState } from '../default';
 
 const useInformation = () => {
   const dispatch = useDispatch();
   const state = useSelectState().information;
-  const educationalStatus = useSelectState().selectType.educationalStatus;
-  let autoSaveInformationState;
-  const autoSaveState = {
-    name: state.name,
-    gender: state.gender,
-    birthYear: state.birthYear,
-    birthMonth: state.birthMonth,
-    birthDate: state.birthDate,
-    schoolCode: state.schoolCode,
-    schoolPhoneNumber: state.schoolPhoneNumber,
-    parentName: state.parentName,
-    parentPhoneNumber: state.parentPhoneNumber,
-    phoneNumber: state.phoneNumber,
-    homePhoneNumber: state.homePhoneNumber,
-    baseAddress: state.baseAddress,
-    detailAddress: state.detailAddress,
-    zipCode: state.zipCode,
-    stdGrade: state.stdGrade,
-    stdClass: state.stdClass,
-    stdNumber: state.stdNumber,
-  };
-  const autoSaveGedState = {
-    name: state.name,
-    gender: state.gender,
-    birthYear: state.birthYear,
-    birthMonth: state.birthMonth,
-    birthDate: state.birthDate,
-    parentName: state.parentName,
-    parentPhoneNumber: state.parentPhoneNumber,
-    phoneNumber: state.phoneNumber,
-    homePhoneNumber: state.homePhoneNumber,
-    baseAddress: state.baseAddress,
-    detailAddress: state.detailAddress,
-    zipCode: state.zipCode,
-    totalScore: state.totalScore,
-  };
-
-  let autoSaveInformationRequest;
-  if (educationalStatus === 'QUALIFICATION_EXAM') {
-    autoSaveInformationRequest = autoSaveGedInformation;
-    autoSaveInformationState = autoSaveState;
-  } else {
-    autoSaveInformationRequest = autoSaveInformation;
-    autoSaveInformationState = autoSaveGedState;
-  }
   const setState = {
     setInput: (payload: { name: string; value: string }) => {
       dispatch(setInput(payload));
-      dispatch(
-        autoSaveInformationRequest({
-          ...autoSaveInformationState,
-          [payload.name]: payload.value,
-        }),
-      );
     },
-    setGender: (payload: string) => {
-      dispatch(
-        autoSaveInformationRequest({
-          ...autoSaveInformationState,
-          gender: payload,
-        }),
-      );
-      dispatch(setGender(payload));
+    setSex: (payload: string) => {
+      dispatch(setSex(payload));
     },
     setBirthYear: (payload: number) => {
-      dispatch(
-        autoSaveInformationRequest({
-          ...autoSaveInformationState,
-          birthYear: payload,
-        }),
-      );
       dispatch(setBirthYear(payload));
     },
     setBirthMonth: (payload: number) => {
-      dispatch(
-        autoSaveInformationRequest({
-          ...autoSaveInformationState,
-          birthMonth: payload,
-        }),
-      );
       dispatch(setBirthMonth(payload));
     },
     setBirthDate: (payload: number) => {
-      dispatch(
-        autoSaveInformationRequest({
-          ...autoSaveInformationState,
-          birthDate: payload,
-        }),
-      );
       dispatch(setBirthDate(payload));
     },
     setImageUrl: (payload: string) => {
       dispatch(setImageUrl(payload));
     },
-    setImageFile: (payload: File) => {
-      dispatch(setImageFile(payload));
-    },
     setAddress: (payload: addressType) => {
-      dispatch(
-        autoSaveInformationRequest({
-          ...autoSaveInformationState,
-          baseAddress: payload.baseAddress,
-          zipCode: payload.zipCode,
-        }),
-      );
       dispatch(setAddress(payload));
     },
-    information: (payload: informationType) => {
-      dispatch(information(payload));
+    setSchoolCode: (payload: string) => {
+      dispatch(setSchoolCode(payload));
     },
-    autoSaveInformation: (payload: informationType) => {
-      dispatch(autoSaveInformation(payload));
+    setUserPicture: (payload: File) => {
+      dispatch(userPicture(payload));
     },
-    gedInformation: (payload: gedInformationType) => {
-      dispatch(gedInformation(payload));
-    },
-    informationImage: (payload: File) => {
-      dispatch(informationImage(payload));
-    },
-    searchSchool: (payload: { schoolSearchName: string; size: number; page: number }) => {
+    searchSchool: (payload: searchSchoolQueryType) => {
       dispatch(searchSchool(payload));
     },
-    setSchoolCode: (payload: string) => {
-      dispatch(
-        autoSaveInformation({
-          ...autoSaveState,
-          schoolCode: payload,
-        }),
-      );
-      dispatch(setSchoolCode(payload));
+    setSchoolName: (payload: string) => {
+      dispatch(setSchoolName(payload));
     },
   };
 
