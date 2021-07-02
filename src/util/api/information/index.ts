98b0111ef59aed80a22169d22e8896bc9a42425f
hyejun <hyejun0608@naver.com>
2021-07-02 21:46:14 +0900

    [FIX] 새로운 서버 코드를 구축

--- src/util/api/information/index.ts
+++ src/util/api/information/index.ts
@@ -1,124 +1,65 @@
-import { gedInformationType, informationType } from 'src/constance/information';
+import {
+  graduateInformationInterFace,
+  informationInterface,
+  searchSchoolQueryType,
+} from '../../../constance/information';
+import { reducerType } from '../../../modules/redux/reducer';
 import uri from '../../../constance/uri';
 import { getRequestWithAccessToken } from '../default';
 
-export const information = async (access_token: string, informationRequest: informationType) => {
+const makeBirthday = (year: number, month: number, date: number) => {
+  if (month < 10) {
+    if (date < 10) return `${year}0${month}0${date}`;
+    else return `${year}0${month}${date}`;
+  } else {
+    if (date < 10) return `${year}${month}0${date}`;
+    else return `${year}${month}${date}`;
+  }
+};
+
+const makeStudentNumber = (stdGrade: string, stdClass: string, stdNumber: string) => {
+  if (stdNumber.length === 1) {
+    return `${stdGrade}${stdClass}0${stdNumber}`;
+  } else return `${stdGrade}${stdClass}${stdNumber}`;
+};
+
+export const informationStateToRequest = (
+  state: reducerType['information'],
+): informationInterface => {
+  return {
+    name: state.userName,
+    sex: state.sex,
+    birthday: makeBirthday(state.birthYear, state.birthMonth, state.birthDate),
+    parent_name: state.parentName,
+    parent_tel: state.parentTel,
+    telephone_number: state.telephoneNumber,
+    home_tel: state.homeTel,
+    address: state.address,
+    detail_address: state.detailAddress,
+    post_code: state.postCode,
+  };
+};
+
+export const information = async (
+  access_token: string,
+  informationRequest: informationInterface,
+) => {
   try {
     const request = getRequestWithAccessToken(access_token);
-    let grade = '';
-    if (informationRequest.stdNumber.length === 1) {
-      grade = `${informationRequest.stdGrade}${informationRequest.stdClass}0${informationRequest.stdNumber}`;
-    } else
-      grade = `${informationRequest.stdGrade}${informationRequest.stdClass}${informationRequest.stdNumber}`;
-    let birthDay = '';
-    if (informationRequest.birthMonth < 10) {
-      if (informationRequest.birthDate < 10)
-        birthDay = `${informationRequest.birthYear}0${informationRequest.birthMonth}0${informationRequest.birthDate}`;
-      else
-        birthDay = `${informationRequest.birthYear}0${informationRequest.birthMonth}${informationRequest.birthDate}`;
-    } else {
-      if (informationRequest.birthDate < 10)
-        birthDay = `${informationRequest.birthYear}${informationRequest.birthMonth}0${informationRequest.birthDate}`;
-      else
-        birthDay = `${informationRequest.birthYear}${informationRequest.birthMonth}${informationRequest.birthDate}`;
-    }
-    return await request.patch(uri.information, {
-      name: informationRequest.name,
-      sex: informationRequest.gender,
-      birthday: birthDay,
-      school_code: informationRequest.schoolCode,
-      school_tel: informationRequest.schoolPhoneNumber,
-      parent_name: informationRequest.parentName,
-      parent_tel: informationRequest.parentPhoneNumber,
-      telephone_number: informationRequest.phoneNumber,
-      home_tel: informationRequest.homePhoneNumber,
-      address: informationRequest.baseAddress,
-      detail_address: informationRequest.detailAddress,
-      post_code: informationRequest.zipCode,
-      student_number: grade,
-    });
+    await request.patch(uri.information, informationRequest);
   } catch (error) {
     throw error;
   }
 };
 
-export const gedInformation = async (
-  access_token: string,
-  gedInformationRequest: gedInformationType,
-) => {
-  try {
-    const request = getRequestWithAccessToken(access_token);
-    let birthDay = '';
-    if (gedInformationRequest.birthMonth < 10) {
-      if (gedInformationRequest.birthDate < 10)
-        birthDay = `${gedInformationRequest.birthYear}0${gedInformationRequest.birthMonth}0${gedInformationRequest.birthDate}`;
-      else
-        birthDay = `${gedInformationRequest.birthYear}0${gedInformationRequest.birthMonth}${gedInformationRequest.birthDate}`;
-    } else {
-      if (gedInformationRequest.birthDate < 10)
-        birthDay = `${gedInformationRequest.birthYear}${gedInformationRequest.birthMonth}0${gedInformationRequest.birthDate}`;
-      else
-        birthDay = `${gedInformationRequest.birthYear}${gedInformationRequest.birthMonth}${gedInformationRequest.birthDate}`;
-    }
-    await request.patch(uri.gedInformation, {
-      name: gedInformationRequest.name,
-      sex: gedInformationRequest.gender,
-      birthday: birthDay,
-      parent_name: gedInformationRequest.parentName,
-      parent_tel: gedInformationRequest.parentPhoneNumber,
-      telephone_number: gedInformationRequest.phoneNumber,
-      home_tel: gedInformationRequest.homePhoneNumber,
-      address: gedInformationRequest.baseAddress,
-      detail_address: gedInformationRequest.detailAddress,
-      post_code: gedInformationRequest.zipCode,
-      average_score: Number((gedInformationRequest.totalScore / 6).toFixed(2)),
-    });
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const autoSaveInformation = async (
-  access_token: string,
-  informationRequest: informationType,
-) => {
-  try {
-    const request = getRequestWithAccessToken(access_token);
-    let grade = '';
-    if (informationRequest.stdNumber.length === 1) {
-      grade = `${informationRequest.stdGrade}${informationRequest.stdClass}0${informationRequest.stdNumber}`;
-    } else
-      grade = `${informationRequest.stdGrade}${informationRequest.stdClass}${informationRequest.stdNumber}`;
-    let birthDay = '';
-    if (informationRequest.birthMonth < 10) {
-      if (informationRequest.birthDate < 10)
-        birthDay = `${informationRequest.birthYear}0${informationRequest.birthMonth}0${informationRequest.birthDate}`;
-      else
-        birthDay = `${informationRequest.birthYear}0${informationRequest.birthMonth}${informationRequest.birthDate}`;
-    } else {
-      if (informationRequest.birthDate < 10)
-        birthDay = `${informationRequest.birthYear}${informationRequest.birthMonth}0${informationRequest.birthDate}`;
-      else
-        birthDay = `${informationRequest.birthYear}${informationRequest.birthMonth}${informationRequest.birthDate}`;
-    }
-    return await request.patch(uri.information, {
-      name: informationRequest.name,
-      sex: informationRequest.gender,
-      birthday: birthDay,
-      school_code: informationRequest.schoolCode,
-      school_tel: informationRequest.schoolPhoneNumber,
-      parent_name: informationRequest.parentName,
-      parent_tel: informationRequest.parentPhoneNumber,
-      telephone_number: informationRequest.phoneNumber,
-      home_tel: informationRequest.homePhoneNumber,
-      address: informationRequest.baseAddress,
-      detail_address: informationRequest.detailAddress,
-      post_code: informationRequest.zipCode,
-      student_number: grade,
-    });
-  } catch (error) {
-    throw error;
-  }
+export const graduateInformationStateToRequest = (
+  state: reducerType['information'],
+): graduateInformationInterFace => {
+  return {
+    student_number: makeStudentNumber(state.stdGrade, state.stdClass, state.stdNumber),
+    school_code: state.schoolCode,
+    school_tel: state.schoolTel,
+  };
 };
 
 export const getInformation = async (access_token: string) => {
@@ -130,70 +71,49 @@ export const getInformation = async (access_token: string) => {
   }
 };
 
-export const getGedInformation = async (access_token: string) => {
-  try {
-    const request = getRequestWithAccessToken(access_token);
-    return console.log(await request.get(uri.gedInformation));
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const informationImage = async (access_token: string, informationImageRequest: File) => {
-  const formData = new FormData();
-  formData.append('file', informationImageRequest);
-  try {
-    const request = getRequestWithAccessToken(access_token);
-    return await request.post(uri.informationImage, formData);
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const searchSchool = async (access_token: string, searchSchoolRequest) => {
-  try {
-    const request = getRequestWithAccessToken(access_token);
-    return await request.get(
-      `${uri.searchSchool}?name=${encodeURI(searchSchoolRequest.schoolSearchName)}&size=${
-        searchSchoolRequest.size
-      }&page=${searchSchoolRequest.page}`,
-    );
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const autoSaveGedInformation = async (
+export const graduateInformation = async (
   access_token: string,
-  informationRequest: gedInformationType,
+  graduateInformationRequest: graduateInformationInterFace,
 ) => {
   try {
     const request = getRequestWithAccessToken(access_token);
-    let birthDay = '';
-    if (informationRequest.birthMonth < 10) {
-      if (informationRequest.birthDate < 10)
-        birthDay = `${informationRequest.birthYear}0${informationRequest.birthMonth}0${informationRequest.birthDate}`;
-      else
-        birthDay = `${informationRequest.birthYear}0${informationRequest.birthMonth}${informationRequest.birthDate}`;
-    } else {
-      if (informationRequest.birthDate < 10)
-        birthDay = `${informationRequest.birthYear}${informationRequest.birthMonth}0${informationRequest.birthDate}`;
-      else
-        birthDay = `${informationRequest.birthYear}${informationRequest.birthMonth}${informationRequest.birthDate}`;
-    }
-    return await request.patch(uri.information, {
-      name: informationRequest.name,
-      sex: informationRequest.gender,
-      birthday: birthDay,
-      totalScore: informationRequest.totalScore,
-      parent_name: informationRequest.parentName,
-      parent_tel: informationRequest.parentPhoneNumber,
-      telephone_number: informationRequest.phoneNumber,
-      home_tel: informationRequest.homePhoneNumber,
-      address: informationRequest.baseAddress,
-      detail_address: informationRequest.detailAddress,
-      post_code: informationRequest.zipCode,
-    });
+    await request.patch(uri.graduateInformation, graduateInformationRequest);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getGraduateInformation = async (access_token: string) => {
+  try {
+    const request = getRequestWithAccessToken(access_token);
+    return await request.get(uri.graduateInformation);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const userPhoto = async (access_token: string, userPhotoRequest: File) => {
+  try {
+    const formData = new FormData();
+    formData.append('file', userPhotoRequest);
+    const request = getRequestWithAccessToken(access_token);
+    return await request.post(uri.userPhoto, formData);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getSearchSchool = async (
+  access_token: string,
+  searchSchoolQuery: searchSchoolQueryType,
+) => {
+  try {
+    const request = getRequestWithAccessToken(access_token);
+    return await request.get(
+      `${uri.searchSchool}?name=${encodeURI(searchSchoolQuery.name)}&size=${
+        searchSchoolQuery.size
+      }&page=${searchSchoolQuery.page}`,
+    );
   } catch (error) {
     throw error;
   }
