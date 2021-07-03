84616b5e8a51e195b81aa82f0b467d56ef7cdc3a
hyejun <hyejun0608@naver.com>
2021-07-03 15:52:29 +0900

    [FIX] 졸업자, 졸업예정자, 검정고시에 따라 정보를 불러오는 api 호출

--- src/containers/information/InformationContainer.tsx
+++ src/containers/information/InformationContainer.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import useInformation from '../../util/hooks/information';
 import Information from '../../components/information';
 import { useDispatch } from 'react-redux';
 import { GET_SELECTTYPE } from '../../modules/redux/action/selectType/interface';
 import useSelectType from '../../util/hooks/selectType';
 import {
+  GET_GED_SCORE,
   GET_GRADUATE_INFORMATION,
   GET_INFORMATION,
 } from '../../modules/redux/action/information/interface';
@@ -13,14 +14,25 @@ const InformationContainer = () => {
   const { state, setState } = useInformation();
   const educationalStatus = useSelectType().state.educationalStatus;
   const dispatch = useDispatch();
+  const mounted = useRef(false);
 
   useEffect(() => {
     dispatch({ type: GET_SELECTTYPE });
     dispatch({ type: GET_INFORMATION });
-    if (educationalStatus !== 'QUALIFICATION_EXAM') {
-      dispatch({ type: GET_GRADUATE_INFORMATION });
-    }
   }, []);
+
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true;
+    } else {
+      if (educationalStatus === 'QUALIFICATION_EXAM') {
+        dispatch({ type: GET_GED_SCORE });
+      } else {
+        dispatch({ type: GET_GRADUATE_INFORMATION });
+      }
+    }
+  }, [educationalStatus]);
+
   return <Information {...state} {...setState} />;
 };
 
