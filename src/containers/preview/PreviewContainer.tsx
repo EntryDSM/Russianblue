14616eb9922980680d71d7604014ea8ad46742ed
hyejun <hyejun0608@naver.com>
2021-07-05 15:58:46 +0900

    [FIX] 렌더링 될 때 지원자 pdf 출력 api 실행

--- src/containers/preview/PreviewContainer.tsx
+++ src/containers/preview/PreviewContainer.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { usePreview } from '../../util/hooks/preview';
 import Preview from '../../components/preview';
+import { useDispatch } from 'react-redux';
+import { GET_PREVIEW } from '../../modules/redux/action/preview/interface';
 
 const PreviewContainer = () => {
   const { state, setState } = usePreview();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: GET_PREVIEW });
+  }, []);
+
   return <Preview {...state} {...setState} />;
 };
 
