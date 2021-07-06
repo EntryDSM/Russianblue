0b5b0f86b648e88bab377832da79aa8a3c535989
Eungyeole <eungyeole@naver.com>
2021-07-06 14:53:35 +0900

    [FIX] preview의 action, reducer, interface 수정

--- src/modules/redux/action/preview/index.ts
+++ src/modules/redux/action/preview/index.ts
@@ -1,6 +1,12 @@
-import { PREVIEW } from './interface';
+import { error } from '../../../../models/error';
+import { GET_PREVIEW, GET_PREVIEW_SUCCESS, GET_PREVIEW_FAILURE } from './interface';
 import { createAction } from 'typesafe-actions';
 
-export const setPreview = createAction(PREVIEW)<string>();
+export const getPreview = createAction(GET_PREVIEW)();
+export const getPreviewSuccess = createAction(GET_PREVIEW_SUCCESS)<string>();
+export const getPreviewFailure = createAction(GET_PREVIEW_FAILURE)<error>();
 
-export type previewActionType = ReturnType<typeof setPreview>;
+export type previewActionType =
+  | ReturnType<typeof getPreview>
+  | ReturnType<typeof getPreviewSuccess>
+  | ReturnType<typeof getPreviewFailure>;
