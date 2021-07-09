1b4ff38e521db4425ba146e3addbf3889809c6d2
hyejun <hyejun0608@naver.com>
2021-07-09 11:19:26 +0900

    [FIX] 최종제출 하면 다시 접근 불가

--- src/containers/selectType/SelectTypeContainer.tsx
+++ src/containers/selectType/SelectTypeContainer.tsx
@@ -4,14 +4,21 @@ import { GET_SELECTTYPE } from '../../modules/redux/action/selectType/interface'
 import SelectType from '../../components/SelectType';
 import useSelectType from '../../util/hooks/selectType';
 import { useIsLogin } from '../../util/api/default';
+import { useHistory } from 'react-router-dom';
+import { useUser } from '../../util/hooks/user';
 
 const SelectTypeContainer = () => {
   const { state, setState } = useSelectType();
+  const isFinalSubmitDone = useUser().state.isfinalSubmitDone;
   const dispatch = useDispatch();
+  const history = useHistory();
   useIsLogin();
   useEffect(() => {
     dispatch({ type: GET_SELECTTYPE });
   }, []);
+  useEffect(() => {
+    if (isFinalSubmitDone) history.push('/');
+  }, [isFinalSubmitDone]);
   return <SelectType {...state} {...setState} />;
 };
 
