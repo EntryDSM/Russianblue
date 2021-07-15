5533e74e9aad7499899c3d481591248a1311b30a
hyejun <hyejun0608@naver.com>
2021-07-15 16:11:06 +0900

    [FIX] 서버요청으로 들어온 전화번호도 - 붙여 보여줌

--- src/components/information/column/PhoneNumberColumn.tsx
+++ src/components/information/column/PhoneNumberColumn.tsx
@@ -36,6 +36,9 @@ const PhoneNumberColumn: FC<Props> = ({
         if (number.length === 10) {
           return number.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
         }
+        if (number.length === 11) {
+          return number.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3');
+        }
         if (number.length === 13) {
           return number.replace(/-/g, '').replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3');
         }
