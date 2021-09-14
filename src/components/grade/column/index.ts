a0024d35923a3d2e920a0bc09f2259611d3c95d3
hyejun <hyejun0608@naver.com>
2021-09-14 21:47:21 +0900

    [DELETE] 미이수 학기 체크 사라짐

--- src/components/grade/column/index.ts
+++ src/components/grade/column/index.ts
@@ -1,5 +1,4 @@
 export { default as VolunteerColumn } from './VolunteerColumn';
 export { default as GradeColumn } from './GradeColumn';
-export { default as CheckColumn } from './CheckColumn';
 export { default as GradeTr } from './GradeTr';
 export { default as ResetGrade } from './ResetGrade';
