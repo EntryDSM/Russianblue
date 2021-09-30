808a66fdb4e8ca58a182d6fe4961a70d4caaa9c0
hyejun <hyejun0608@naver.com>
2021-09-30 09:31:51 +0900

    [FIX] 모의 접수를 위한 문구 변경

--- src/modules/redux/reducer/status/mainConstance.tsx
+++ src/modules/redux/reducer/status/mainConstance.tsx
@@ -28,8 +28,10 @@ export type statusType =
 
 const mainConstance: Record<statusType, processType> = {
   [NOT_APPLICATION_PERIOD]: {
-    title: <p>지금은 원서접수기간이 아닙니다.</p>,
-    getDescription: (date: string) => <p>원서접수 기간은 {<span>{date}</span>}에 시작됩니다.</p>,
+    title: <p>지금은 원서 모의 접수기간이 아닙니다.</p>,
+    getDescription: (date: string) => (
+      <p>원서 모의 접수 기간은 {<span>{date}</span>}에 시작됩니다.</p>
+    ),
     isHaveTerm: true,
     buttonText: '원서 작성',
     isButtonAble: false,
@@ -115,8 +117,10 @@ const mainConstance: Record<statusType, processType> = {
     isOutsideUrl: true,
   },
   [APPLICATION_PERIOD]: {
-    title: <p>지금은 {<span>원서 작성</span>} 기간입니다.</p>,
-    getDescription: (date: string) => <p>원서 접수 기간은 {<span>{date}</span>}까지 입니다.</p>,
+    title: <p>지금은 {<span>원서 모의 작성</span>} 기간입니다.</p>,
+    getDescription: (date: string) => (
+      <p>원서 모의 접수 기간은 {<span>{date}</span>}까지 입니다.</p>
+    ),
     isHaveTerm: true,
     buttonText: '원서 작성',
     isButtonAble: true,
