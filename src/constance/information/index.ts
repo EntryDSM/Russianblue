fb4ce69597d42c9b93132e123162e63b920dca58
hyejun <hyejun0608@naver.com>
2021-10-05 11:06:49 +0900

    [FIX] 용량 크기 문구 추가

--- src/constance/information/index.ts
+++ src/constance/information/index.ts
@@ -3,6 +3,7 @@ export const INFORMATIONTITLE = '2022년 지원자 인적사항';
 export const EXPLAIN = "'-'문자를 제외한 숫자만 입력해주세요.";
 export const IMG_EXPLAIN = '증명사진을 첨부해주세요';
 export const IMG_FORMATS = '(JPG,JPEG,JPEG2000,PNG)';
+export const IMG_SIZE = '용량 10MB이하';
 export const GENDER = [
   { id: 'male', content: '남자' },
   { id: 'female', content: '여자' },
